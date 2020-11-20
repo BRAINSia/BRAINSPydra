6d2986386f6508a3b3c802de0d9e8050073f0ab2
Charles E Johnson III <cjohnson30@argon-lc-h21-31.hpc>
2020-11-20 09:27:29 -0600

    Removed the __init__ files causing relative imports to fail

--- BIDSFilename.py
+++ BIDSFilename.py
@@ -1,21 +1,32 @@
 import os
 from collections import OrderedDict
 
-        
+
 class BIDSFilename(object):
     def __init__(self, raw_filename):
         self.__full_raw_filename = os.path.abspath(raw_filename)
         self.__dirname = os.path.dirname(self.__full_raw_filename)
         self.__basename = os.path.basename(self.__full_raw_filename)
-        self.extension = '.'.join(self.__basename.split('.')[1:])
+        self.extension = ".".join(self.__basename.split(".")[1:])
 
-        # convenient representation 
-        attribute_list_of_lists = [elem.split("-") for elem in self.__basename.split("_")]   
+        # convenient representation
+        attribute_list_of_lists = [
+            elem.split("-") for elem in self.__basename.split("_")
+        ]
 
-        self.attribute_dict = { sub_list[0]: sub_list[1] for sub_list in attribute_list_of_lists if len(sub_list) == 2 }
-        self.attributes_wo_keys = [sub_list[0].replace(f".{self.extension}", "") for sub_list in attribute_list_of_lists if len(sub_list) == 1]
-        self.attribute_order = [sub_list[0] for sub_list in attribute_list_of_lists if len(sub_list) == 2]
-
+        self.attribute_dict = {
+            sub_list[0]: sub_list[1]
+            for sub_list in attribute_list_of_lists
+            if len(sub_list) == 2
+        }
+        self.attributes_wo_keys = [
+            sub_list[0].replace(f".{self.extension}", "")
+            for sub_list in attribute_list_of_lists
+            if len(sub_list) == 1
+        ]
+        self.attribute_order = [
+            sub_list[0] for sub_list in attribute_list_of_lists if len(sub_list) == 2
+        ]
 
     def __getattr__(self, attr_key):
         return self.attribute_dict[attr_key]
@@ -23,17 +34,22 @@ class BIDSFilename(object):
     # Generate new filename
     def get_basename(self):
         # sort the attribute dictionary using the attribute_order dictionary
-        ordered_attr_dict = OrderedDict(sorted(self.attribute_dict.items(), key=lambda x: self.attribute_order.index(x[0])))
+        ordered_attr_dict = OrderedDict(
+            sorted(
+                self.attribute_dict.items(),
+                key=lambda x: self.attribute_order.index(x[0]),
+            )
+        )
         # join the dictionary
-        keyed_attr_string = "_".join(["-".join([k, v]) for k, v in ordered_attr_dict.items()])
+        keyed_attr_string = "_".join(
+            ["-".join([k, v]) for k, v in ordered_attr_dict.items()]
+        )
         # join all of the attrs w/o keys
         non_keyed_attr_string = "_".join(self.attributes_wo_keys)
         return f"{keyed_attr_string}_{non_keyed_attr_string}.{self.extension}"
 
-
     def __str__(self):
         return os.path.join(self.__dirname, self.get_basename())
-
 
     # Create, Update, Delete attribute pairs
     def add_attribute(self, attr_key, attr_value):
@@ -49,7 +65,7 @@ class BIDSFilename(object):
             self.attribute_dict[attr_key] = attr_value
         except AssertionError as e:
             raise ValueError(f"Key {attr_key} cannot be updated as it is not in dict")
-    
+
     def delete_attribute(self, attr_key):
         try:
             assert attr_key in self.attribute_dict.keys()
@@ -57,4 +73,3 @@ class BIDSFilename(object):
             del self.attribute_order[self.attribute_order.index(attr_key)]
         except AssertionError as e:
             raise ValueError(f"Key {attr_key} not in attribute_dict")
-    
