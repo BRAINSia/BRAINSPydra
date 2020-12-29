88565b8a37512e54b7932ab2f51a2d3fd0287bbf
Chase Johnson <chaser.johnson27@gmail.com>
2020-12-29 15:37:12 -0600

    Created the structure of a new pipeline designed to be generic.

--- wf_resample_bcd.py
+++ wf_resample_bcd.py
@@ -26,67 +26,16 @@ if __name__ == '__main__':
 
     resample = BRAINSResample()
     bcd = BRAINSConstellationDetector()
-    bcd.task = fill_bcd_task()
+    wf = pydra.Workflow(name="wf", input_spec=["t1", "ref", "transform"], output_spec=["outputDir"])
+    wf.inputs.t1 = subject1_json['in']['t1']
+    wf.inputs.ref = subject1_json['in']['ref']
+    wf.inputs.transform = subject1_json['in']['transform']
+    
+    
 
-    resample.task.inputs.inputVolume = subject1_json["in"]["t1"]
-    resample.task.inputs.referenceVolume = subject1_json["in"]["ref"]
-    resample.task.inputs.warpTransform = subject1_json["in"]["transform"]
-    
- 
-    wf = pydra.Workflow(name="wf", input_spec=["cmd1", "cmd2"])
-    
-    wf.inputs.cmd1 = "BRAINSResample"
-    wf.inputs.cmd2 = "BRAINSConstellationDetector"
+    wf.add(resample.task)
+    wf.add(bcd.task)
    
-    # Set the inputs for Resample 
-    resample.task.inputs.interpolationMode = "Linear"
-    resample.task.inputs.pixelType = "binary"
-    resample.task.inputs.outputVolume = f"/localscratch/Users/cjohnson30/output_dir/sub-052823_ses-43817_run-002_T1w_resampled.nii.gz" 
-    resample.task.inputs.outputVolume = [
-        f"/localscratch/Users/cjohnson30/output_dir/{Path(x).with_suffix('').with_suffix('').name}_resampled.nii.gz"
-        for x in subject1_json['in']['t1']
-    ]
-    # Add the resample task to the pipeline 
-    wf.add(resample.task.split(("inputVolume", "outputVolume")))
-    
-    # Set the inputs for BCD                  
-    bcd.task.inputs.inputTemplateModel = "/Shared/sinapse/CACHE/20200915_PREDICTHD_base_CACHE/Atlas/20141004_BCD/T1_50Lmks.mdl",
-    bcd.task.inputs.LLSModel = "/Shared/sinapse/CACHE/20200915_PREDICTHD_base_CACHE/Atlas/20141004_BCD/LLSModel_50Lmks.h5",
-    bcd.task.inputs.acLowerBound = 80.000000,
-    bcd.task.inputs.atlasLandmarkWeights = "/Shared/sinapse/CACHE/20200915_PREDICTHD_base_CACHE/Atlas/20141004_BCD/template_weights_50Lmks.wts",
-    bcd.task.inputs.atlasLandmarks = "/Shared/sinapse/CACHE/20200915_PREDICTHD_base_CACHE/Atlas/20141004_BCD/template_landmarks_50Lmks.fcsv",
-    bcd.task.inputs.houghEyeDetectorMode = 1,
-    bcd.task.inputs.interpolationMode = "Linear" 
-    bcd.task.inputs.resultsDir = subject1_json["out"]["output_dir"]
-#    bcd.task.inputs.inputVolume = wf.BRAINSResample.lzout.outputVolume
-#    bcd.task.inputs.outputLandmarksInInputSpace =  [
-#        f"{bcd.task.inputs.resultsDir}/{Path(x).with_suffix('').with_suffix('').name}_BCD_Original.fcsv"
-#        for x in subject1_json['in']['t1'] 
-#    ]
-#    bcd.task.inputs.outputResampledVolume = [
-#        f"{bcd.task.inputs.resultsDir}/{Path(x).with_suffix('').with_suffix('').name}_BCD_ACPC.nii.gz"
-#        for x in subject1_json['in']['t1']
-#    ]
-#    bcd.task.inputs.outputTransform = [
-#        f"{bcd.task.inputs.resultsDir}/{Path(x).with_suffix('').with_suffix('').name}_BCD_Original2ACPC_transform.h5"
-#        for x in subject1_json['in']['t1']
-#    ]
-#    bcd.task.inputs.outputLandmarksInACPCAlignedSpace = [
-#        f"{bcd.task.inputs.resultsDir}/{Path(x).with_suffix('').with_suffix('').name}_BCD_ACPC_Landmarks.fcsv"
-#        for x in subject1_json['in']['t1']
-#    ]
-#    bcd.task.inputs.outputLandmarksInInputSpace = f"{bcd.task.inputs.resultsDir}/sub-052823_ses-43817_run-002_T1w_BCD_Original.fcsv"
-#    bcd.task.inputs.outputResampledVolume = f"{bcd.task.inputs.resultsDir}/sub-052823_ses-43817_run-002_T1w_BCD_ACPC.nii.gz"
-#    bcd.task.inputs.outputTransform = f"{bcd.task.inputs.resultsDir}/sub-052823_ses-43817_run-002_T1w_BCD_Original2ACPC_transform.h5"
-#    bcd.task.inputs.outputLandmarksInACPCAlignedSpace = "sub-052823_ses-43817_run-002_T1w_BCD_ACPC_Landmarks.fcsv" #f"{bcd.task.inputs.resultsDir}/sub-052823_ses-43817_run-002_T1w_BCD_ACPC_Landmarks.fcsv"
-    # Add the bcd task to the pipeline
-    wf.add(bcd.task.split((
-            "inputVolume",
-            "outputLandmarksInACPCAlignedSpace",
-            "outputLandmarksInInputSpace",
-            "outputResampledVolume",
-            "outputTransform"))) 
-    
     wf.set_output(
         [
             ("outVol", wf.BRAINSResample.lzout.outputVolume),
