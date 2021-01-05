774069c6d515f199db48d1ebaf143207ca60d6eb
Chase Johnson <chaser.johnson27@gmail.com>
2021-01-05 10:54:17 -0600

    Using all the bcd output filenames.

--- wf_resample_bcd.py
+++ wf_resample_bcd.py
@@ -83,9 +83,9 @@ if __name__ == "__main__":
     bcd.inputs.interpolationMode =                 "Linear"
     bcd.inputs.outputLandmarksInInputSpace =       wf.outputLandmarksInInputSpace.lzout.out 
     bcd.inputs.outputResampledVolume =             wf.outputResampledVolume.lzout.out 
-    bcd.inputs.outputTransform =                   "test.h5"               #wf.outputTransform.lzout.out 
-    bcd.inputs.outputLandmarksInACPCAlignedSpace = "test.fcsv"               #wf.outputLandmarksInACPCAlignedSpace.lzout.out 
-    bcd.inputs.writeBranded2DImage =               "test.png"               #wf.writeBranded2DImage.lzout.out 
+    bcd.inputs.outputTransform =                   wf.outputTransform.lzout.out 
+    bcd.inputs.outputLandmarksInACPCAlignedSpace = wf.outputLandmarksInACPCAlignedSpace.lzout.out 
+    bcd.inputs.writeBranded2DImage =               wf.writeBranded2DImage.lzout.out 
     wf.add(bcd)
 
     # Set the filename of the output of Resample
