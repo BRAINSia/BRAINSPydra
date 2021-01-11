a239ecb0d9c630a0eabbff6e3a4b8033c9f7f183
Chase Johnson <chaser.johnson27@gmail.com>
2021-01-11 11:57:54 -0600

    Returning the output from wf through the sink node.

--- preliminary_pipeline1.py
+++ preliminary_pipeline1.py
@@ -23,16 +23,22 @@ source_node.set_output([("t1", source_node.get_subject.lzout.out)])
 
 wf = pydra.Workflow(name="wf", input_spec=["t1"])
 wf.add(source_node)
-wf.add(append(name="append", t1=wf.source_node.lzout.t1))
-wf.add(append(name="append2", t1=wf.append.lzout.out))
-wf.set_output([("output_in_cache1", wf.append.lzout.out),
-               ("output_in_cache", wf.append2.lzout.out)])
+wf.add(append(name="append1", t1=wf.source_node.lzout.t1))
+wf.add(append(name="append2", t1=wf.append1.lzout.out))
+wf.add(append(name="append3", t1=wf.append2.lzout.out))
+wf.set_output([("output_in_cache1", wf.append1.lzout.out),
+               ("output_in_cache2", wf.append2.lzout.out),
+               ("output_in_cache3", wf.append3.lzout.out)])
 
-sink_node = pydra.Workflow(name="sink_node", input_spec=["output_in_cache"])
+sink_node = pydra.Workflow(name="sink_node", input_spec=["output_in_cache1"])
 sink_node.add(wf)
+
+# sink_node.add(wf)
+# sink_node.add(source_node)
+# # sink_node.split("output_in_cache", output_in_cache=sink_node.wf.lzout.output_in_cache)
 # sink_node.add(get_subject(name="get_subject", subj=sink_node.lzin.output_in_cache))
-sink_node.add(append(name="append", t1=sink_node.lzin.output_in_cache))
-sink_node.set_output([("output_not_in_cache", sink_node.append.lzout.out)])
+# sink_node.add(append(name="append", t1=sink_node.wf.lzout.output_in_cache3))
+sink_node.set_output([("output_not_in_cache", sink_node.wf.lzout.output_in_cache3)])
 #
 #
 # outer.add(source_node)
@@ -45,8 +51,8 @@ sink_node.set_output([("output_not_in_cache", sink_node.append.lzout.out)])
 
 
 with pydra.Submitter(plugin="cf") as sub:
-    sub(wf)
-result=wf.result()
+    sub(sink_node)
+result=sink_node.result()
 # with pydra.Submitter(plugin="cf") as sub:
 #     sub(outer)
 # result=outer.result()
