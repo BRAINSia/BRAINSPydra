42e87303211d8bd29e7d20a0799a1d3855670f94
Chase Johnson <chaser.johnson27@gmail.com>
2021-01-11 13:44:40 -0600

    Created a task that touches a file and then outputs it to an output directory.

--- preliminary_pipeline1.py
+++ preliminary_pipeline1.py
@@ -1,5 +1,6 @@
 import pydra
 import nest_asyncio
+
 nest_asyncio.apply()
 
 @pydra.mark.task
@@ -11,6 +12,7 @@ def append(t1, appended):
     return t1 + appended
 
 t1_list = ["subject1.txt", "subject2.txt"]
+
 
 source_node = pydra.Workflow(name="source_node", input_spec=["t1_list"])
 source_node.split("t1_list", t1_list=t1_list)
