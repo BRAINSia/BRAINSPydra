e7355e99f12934c64415d1a97aa055ee6257013b
Chase Johnson <chaser.johnson27@gmail.com>
2021-01-11 15:37:51 -0600

    Preliminary Pipeline 1

--- preliminary_pipeline1.py
+++ preliminary_pipeline1.py
@@ -17,7 +17,7 @@ source_node.split("t1_list", t1_list=t1_list)
 source_node.add(get_subject(name="get_subject", sub=source_node.lzin.t1_list))
 source_node.set_output([("t1", source_node.get_subject.lzout.out)])
 
-preliminary_workflow1 = pydra.Workflow(name="wf", input_spec=["t1"])
+preliminary_workflow1 = pydra.Workflow(name="preliminary_workflow1", input_spec=["t1"])
 preliminary_workflow1.add(source_node)
 preliminary_workflow1.add(append(name="append1", t1=preliminary_workflow1.source_node.lzout.t1))
 preliminary_workflow1.add(append(name="append2", t1=preliminary_workflow1.append1.lzout.out))
@@ -26,7 +26,7 @@ preliminary_workflow1.set_output([("output_in_cache3", preliminary_workflow1.app
 
 sink_node = pydra.Workflow(name="sink_node", input_spec=["output_in_cache1"])
 sink_node.add(preliminary_workflow1)
-sink_node.add(append(name="append").split("t1", t1=sink_node.wf.lzout.output_in_cache3))
+sink_node.add(append(name="append").split("t1", t1=sink_node.preliminary_workflow1.lzout.output_in_cache3))
 sink_node.set_output([("output_not_in_cache", sink_node.append.lzout.out)])
 
 with pydra.Submitter(plugin="cf") as sub:
