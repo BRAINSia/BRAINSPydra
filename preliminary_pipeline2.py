ba0090ff667547499777ca9efdf4943f6cfc9afc
Chase Johnson <chaser.johnson27@gmail.com>
2021-01-12 13:41:28 -0600

    Returning the output of the workflow through the sink_node.

--- preliminary_pipeline2.py
+++ preliminary_pipeline2.py
@@ -2,6 +2,8 @@ import pydra
 import nest_asyncio
 import attr
 from pathlib import Path
+from shutil import copyfile
+
 nest_asyncio.apply()
 
 @pydra.mark.task
@@ -14,10 +16,26 @@ def read_file(file):
     print(f"Reading {file}")
     with open(file, "r") as f:
         print(f.read())
+    return file
 
 @pydra.mark.task
 def append(t1, appended):
     return t1 + appended
+
+@pydra.mark.task
+def append_filename(filename="", append_str="", extension="", directory=""):
+    print(f"filename: {filename}")
+    new_filename = f"{Path(Path(directory) / Path(Path(filename).with_suffix('').with_suffix('').name))}{append_str}{extension}"
+    return new_filename
+
+@pydra.mark.task
+def copy_from_cache(cache_path, output_dir):
+    print(f"cache_path: {cache_path}")
+    print(f"output_dir: {output_dir}")
+    copyfile(cache_path, Path(output_dir) / Path(cache_path).name)
+    out_path = Path(output_dir) / Path(cache_path).name
+    copyfile(cache_path, out_path)
+    return out_path
 
 # Get the list of two files by the patter subject*.txt images in this directory
 t1_list = []
@@ -66,21 +84,27 @@ preliminary_workflow2 = pydra.Workflow(name="preliminary_workflow2", input_spec=
 preliminary_workflow2.add(source_node)
 preliminary_workflow2.add(pydra.ShellCommandTask(name="BRAINSResample", executable="/mnt/c/2020_Grad_School/Research/BRAINSPydra/BRAINSResample.sh", t1=preliminary_workflow2.source_node.lzout.t1, input_spec=my_input_spec, output_spec=my_output_spec))
 preliminary_workflow2.add(pydra.ShellCommandTask(name="BRAINSConstellationDetector", executable="/mnt/c/2020_Grad_School/Research/BRAINSPydra/BRAINSConstellationDetector.sh", t1=preliminary_workflow2.BRAINSResample.lzout.out, input_spec=my_input_spec, output_spec=my_output_spec))
-preliminary_workflow2.set_output([("processed", preliminary_workflow2.BRAINSResample.lzout.out)])
-#
-# sink_node = pydra.Workflow(name="sink_node", input_spec=["output_in_cache1"])
-# sink_node.add(preliminary_workflow2)
-# sink_node.add(append(name="append", appended="_output").split("t1", t1=sink_node.preliminary_workflow2.lzout.processed))
-# sink_node.set_output([("output", sink_node.append.lzout.out)])
+preliminary_workflow2.set_output([("processed_files", preliminary_workflow2.BRAINSResample.lzout.out)])
+
+sink_node = pydra.Workflow(name="sink_node", input_spec=["processed_files"])
+sink_node.add(preliminary_workflow2)
+sink_node.add(get_subject(name="get_subject", sub=sink_node.preliminary_workflow2.lzout.processed_files))
+# sink_node.split("processed_files", processed_files=sink_node.preliminary_workflow2.lzout.processed_files)
+# # sink_node.inputs.processed_files = sink_node.preliminary_workflow2.lzout.processed_files
+# sink_node.add(copy_from_cache(name="copy_from_cache", cache_path=sink_node.lzin.processed_files, output_dir="/mnt/c/2020_Grad_School/Research/BRAINSPydra/output_dir").split("cache_path"))
+sink_node.set_output([("output_file", sink_node.get_subject.lzout.out)])
+
 #
 # with pydra.Submitter(plugin="cf") as sub:
 #     sub(source_node)
 # result=source_node.result()
-with pydra.Submitter(plugin="cf") as sub:
-    sub(preliminary_workflow2)
-result=preliminary_workflow2.result()
+# print(result)
 # with pydra.Submitter(plugin="cf") as sub:
-#     sub(sink_node)
-# result=sink_node.result()
+#     sub(preliminary_workflow2)
+# result=preliminary_workflow2.result()
+# print(result)
+with pydra.Submitter(plugin="cf") as sub:
+    sub(sink_node)
+result=sink_node.result()
 print(result)
 #
