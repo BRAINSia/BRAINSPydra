7de86159c23e9c73d4ada8a643715330d145b49d
Chase Johnson <chaser.johnson27@gmail.com>
2021-01-12 09:23:11 -0600

    Added a makeshift BRAINSResample function that gets the output from the source node and processes it in parallel.

--- preliminary_pipeline2.py
+++ preliminary_pipeline2.py
@@ -6,7 +6,14 @@ nest_asyncio.apply()
 
 @pydra.mark.task
 def get_subject(sub):
+    # print(sub)
     return sub
+
+@pydra.mark.task
+def read_file(file):
+    print(f"Reading {file}")
+    with open(file, "r") as f:
+        print(f.read())
 
 @pydra.mark.task
 def append(t1, appended):
@@ -17,7 +24,7 @@ t1_list = []
 p = Path("/mnt/c/2020_Grad_School/Research/BRAINSPydra/input_files")
 for t1 in p.glob("subject*.txt"):
     t1_list.append(t1)
-print(t1_list)
+# print(t1_list)
 
 # Put the files into the pydra cache and split them into iterable objects
 source_node = pydra.Workflow(name="source_node", input_spec=["t1_list"])
@@ -25,19 +32,19 @@ source_node.split("t1_list", t1_list=t1_list)
 source_node.add(get_subject(name="get_subject", sub=source_node.lzin.t1_list))
 source_node.set_output([("t1", source_node.get_subject.lzout.out)])
 
-# my_input_spec = pydra.specs.SpecInfo(
-#     name="Input",
-#     fields=[
-#         (
-#             "t1",
-#             attr.ib(
-#                 type=pydra.specs.File,
-#                 metadata={"position": 1, "argstr": "", "help_string": "text", "mandatory": True},
-#                 ),
-#         )
-#     ],
-#     bases=(pydra.specs.ShellSpec,),
-# )
+my_input_spec = pydra.specs.SpecInfo(
+    name="Input",
+    fields=[
+        (
+            "t1",
+            attr.ib(
+                type=pydra.specs.File,
+                metadata={"position": 1, "argstr": "", "help_string": "text", "mandatory": True},
+                ),
+        )
+    ],
+    bases=(pydra.specs.ShellSpec,),
+)
 my_output_spec = pydra.specs.SpecInfo(
     name="Output",
     fields=[
@@ -46,7 +53,7 @@ my_output_spec = pydra.specs.SpecInfo(
             attr.ib(
                 type=pydra.specs.File,
                 metadata={
-                    "output_file_template": "{args}",
+                    "output_file_template": "{t1}",
                     "help_string": "output file",
                 },
             ),
@@ -54,10 +61,10 @@ my_output_spec = pydra.specs.SpecInfo(
     ],
     bases=(pydra.specs.ShellOutSpec,),
 )
-args = ["newfile_1.txt", "newfile_2.txt"]
-preliminary_workflow2 = pydra.Workflow(name="preliminary_workflow2", input_spec=["t1"], t1=source_node.lzout.t1)
+
+preliminary_workflow2 = pydra.Workflow(name="preliminary_workflow2", input_spec=["t1"])
 preliminary_workflow2.add(source_node)
-preliminary_workflow2.add(pydra.ShellCommandTask(name="BRAINSResample", executable="echo", args=args, output_spec=my_output_spec).split("args"))
+preliminary_workflow2.add(pydra.ShellCommandTask(name="BRAINSResample", executable="echo", t1=preliminary_workflow2.source_node.lzout.t1, input_spec=my_input_spec, output_spec=my_output_spec))
 # preliminary_workflow2.add(append(name="BRAINSResample", t1=preliminary_workflow2.source_node.lzout.t1, appended="_resampled"))
 # preliminary_workflow2.add(append(name="BRAINSConstellationDetector", t1=preliminary_workflow2.BRAINSResample.lzout.out, appended="_bcd"))
 preliminary_workflow2.set_output([("processed", preliminary_workflow2.BRAINSResample.lzout.out)])
