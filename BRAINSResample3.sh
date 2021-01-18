db73285ed1d81c6422f1086d746d6f259df4e415
Chase Johnson <chaser.johnson27@gmail.com>
2021-01-18 16:48:24 -0600

    Appending to inputVolume contents rather than creating blank new files.

--- BRAINSResample3.sh
+++ BRAINSResample3.sh
@@ -3,8 +3,10 @@
 
 outputVolume=unset
 
+# Set which arguments will be considered valid arguments
 PARSED_ARGUMENTS=$(getopt -n alphabet -o v:: --long outputVolume:,inputVolume: -- "$@")
 
+# Extract the arguments into variables
 eval set -- "$PARSED_ARGUMENTS"
 while :
 do
@@ -17,7 +19,12 @@ do
 
 echo "creating outputVolume : $outputVolume"
 
+# Create a file for the outputVolume
 echo "touching $outputVolume"
 touch "$outputVolume"
+
+# Add the contents of the inputVolume to the top of outputVolume
 cat "$inputVolume" >> "$outputVolume"
+
+# Append "resampled" to outputVolume
 echo "resampled" >> "$outputVolume"
