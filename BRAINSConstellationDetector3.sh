6f5d39ff9922f0b2f551827488fe914d8f09df10
Chase Johnson <chaser.johnson27@gmail.com>
2021-01-18 13:07:31 -0600

    The contents of the inputVolume are the beginning of every output file for BCD.

--- BRAINSConstellationDetector3.sh
+++ BRAINSConstellationDetector3.sh
@@ -3,12 +3,15 @@
 
 outputVolume=unset
 
-PARSED_ARGUMENTS=$(getopt -n alphabet -o v:: --long outputLandmarksInInputSpace:,outputResampledVolume:,outputTransform:,outputLandmarksInACPCAlignedSpace:,writeBranded2DImage: -- "$@")
+# Set which arguments will be considered valid arguments
+PARSED_ARGUMENTS=$(getopt -n alphabet -o v:: --long inputVolume:,outputLandmarksInInputSpace:,outputResampledVolume:,outputTransform:,outputLandmarksInACPCAlignedSpace:,writeBranded2DImage: -- "$@")
 
+# Extract the arguments into variables
 eval set -- "$PARSED_ARGUMENTS"
 while :
 do
     case "$1" in
+    --inputVolume)   inputVolume="$2"   ; shift 2 ;;
     --outputLandmarksInInputSpace)   outputLandmarksInInputSpace="$2"   ; shift 2 ;;
     --outputResampledVolume)             outputResampledVolume="$2"             ; shift 2 ;;
     --outputTransform) outputTransform="$2" ; shift 2 ;;
@@ -24,12 +27,22 @@ echo "touching $outputTransform"
 echo "touching $outputLandmarksInACPCAlignedSpace"
 echo "touching $writeBranded2DImage"
 
+# Create output files with the names specified
 touch "$outputLandmarksInInputSpace"
 touch "$outputResampledVolume"
 touch "$outputTransform"
 touch "$outputLandmarksInACPCAlignedSpace"
 touch "$writeBranded2DImage"
 
+# Add the contents of the inputVolume to the beginning of each output file
+cat "$inputVolume" >> "$outputLandmarksInInputSpace"
+cat "$inputVolume" >> "$outputResampledVolume"
+cat "$inputVolume" >> "$outputTransform"
+cat "$inputVolume" >> "$outputLandmarksInACPCAlignedSpace"
+cat "$inputVolume" >> "$writeBranded2DImage"
+
+
+# Append "bcd" to each file
 echo "bcd" >> "$outputLandmarksInInputSpace"
 echo "bcd" >> "$outputResampledVolume"
 echo "bcd" >> "$outputTransform"
