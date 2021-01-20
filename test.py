d93b12905da75b7a2f1aebc2c012258b83b51bd9
Chase Johnson <chaser.johnson27@gmail.com>
2021-01-20 12:14:19 -0600

    Extracting the lzout.all_ from the processing node.

--- test.py
+++ test.py
@@ -32,12 +32,14 @@ source_node.inputs.x = [2, 4]
 source_node.split("x")
 
 processing_node = pydra.Workflow(name="processing_node", input_spec=["x"], x=source_node.add2.lzout.out)
-processing_node.add(add2(name="add2", x=processing_node.lzin.x))
-processing_node.set_output([("out", processing_node.add2.lzout.out)])
+processing_node.add(add2(name="add2_1", x=processing_node.lzin.x))
+processing_node.add(add2(name="add2_2", x=processing_node.add2_1.lzout.out))
+processing_node.set_output([("add2_1", processing_node.add2_1.lzout.out),
+                            ("add2_2", processing_node.add2_2.lzout.out)])
 
-sink_node = pydra.Workflow(name="sink_node", input_spec=["x"], x=processing_node.lzout.out)
-sink_node.add(add2(name="add2", x=sink_node.lzin.x))
-sink_node.set_output([("out", sink_node.add2.lzout.out)])
+sink_node = pydra.Workflow(name="sink_node", input_spec=["x"], x=processing_node.lzout.all_)
+sink_node.add(extract_from_outall_dict(name="extract_from_outall_dict", d=sink_node.lzin.x))
+sink_node.set_output([("out", sink_node.extract_from_outall_dict.lzout.out)])
 
 source_node.add(processing_node)
 source_node.add(sink_node)
