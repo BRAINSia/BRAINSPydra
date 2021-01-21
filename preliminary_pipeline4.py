07f7d01283ce344ad5d62c587923a6baa1a5918b
Chase Johnson <chaser.johnson27@gmail.com>
2021-01-21 09:12:52 -0600

    Reworked Resample to use an input_data dict instead of list of files.

--- preliminary_pipeline4.py
+++ preliminary_pipeline4.py
@@ -11,6 +11,17 @@ def append_filename(filename="", before_str="", append_str="", extension="", dir
     new_filename = f"{Path(Path(directory) / Path(before_str+Path(filename).with_suffix('').with_suffix('').name))}{append_str}{extension}"
     return new_filename
 
+@pydra.mark.task
+def get_self(x):
+    print(x)
+    return x
+
+@pydra.mark.task
+def get_input_field(input_dict: dict, field):
+    print(input_dict)
+    print(f'returning: {input_dict[field]}')
+    return input_dict[field]
+
 def make_bcd_workflow(my_source_node: pydra.Workflow) -> pydra.Workflow:
     # from .sem_tasks.segmentation.specialized import BRAINSConstellationDetector
     from sem_tasks.segmentation.specialized import BRAINSConstellationDetector
@@ -56,14 +67,17 @@ def make_bcd_workflow(my_source_node: pydra.Workflow) -> pydra.Workflow:
 def make_resample_workflow(my_source_node: pydra.Workflow) -> pydra.Workflow:
     from sem_tasks.registration import BRAINSResample
 
-    resample_workflow = pydra.Workflow(name="resample_workflow", input_spec=["t1"], t1=my_source_node.lzin.t1_list)
+    resample_workflow = pydra.Workflow(name="resample_workflow", input_spec=["input_data"], input_data=my_source_node.lzin.input_data)
+
+    # Get the t1 image for the subject in the input data
+    resample_workflow.add(get_input_field(name="get_t1", input_dict=resample_workflow.lzin.input_data, field="t1"))
 
     # Set the filename of the output of Resample
-    resample_workflow.add(append_filename(name="resampledOutputVolume", filename=resample_workflow.lzin.t1, append_str="_resampled", extension=".nii.gz"))
+    resample_workflow.add(append_filename(name="resampledOutputVolume", filename=resample_workflow.get_t1.lzout.out, append_str="_resampled", extension=".nii.gz"))
 
     # Set the inputs of Resample
     resample_task = BRAINSResample("BRAINSResample", executable=experiment_configuration['BRAINSResample']['executable']).get_task()
-    resample_task.inputs.inputVolume = resample_workflow.lzin.t1
+    resample_task.inputs.inputVolume = resample_workflow.get_t1.lzout.out
     resample_task.inputs.interpolationMode = experiment_configuration["BRAINSResample"]["interpolationMode"]
     resample_task.inputs.pixelType = experiment_configuration["BRAINSResample"]["pixelType"]
     resample_task.inputs.referenceVolume = experiment_configuration["BRAINSResample"]["referenceVolume"]
@@ -72,6 +86,7 @@ def make_resample_workflow(my_source_node: pydra.Workflow) -> pydra.Workflow:
     resample_workflow.add(resample_task)
 
     resample_workflow.set_output([("outputVolume", resample_workflow.BRAINSResample.lzout.outputVolume)])
+
     return resample_workflow
 
 def make_ROIAuto_workflow(my_source_node: pydra.Workflow) -> pydra.Workflow:
@@ -79,7 +94,8 @@ def make_ROIAuto_workflow(my_source_node: pydra.Workflow) -> pydra.Workflow:
 
     roi_workflow = pydra.Workflow(name="roi_workflow", input_spec=["t1"], t1=my_source_node.lzin.t1_list)
 
-    roi_workflow.add(append_filename(name="roiOutputVolume", filename=roi_workflow.lzin.t1, before_str="Cropped_", append_str="_Aligned", extension=".nii.gz"))
+
+    roi_workflow.add(append_filename(name="roiOutputVolume", filename=roi_workflow.lzin.t1, before_str="Cropped_", append_str="_Aligned", extension=".txt"))
 
     roi_task = BRAINSROIAuto("BRAINSROIAuto", executable=experiment_configuration['BRAINSROIAuto']['executable']).get_task()
     roi_task.inputs.inputVolume = roi_workflow.lzin.t1
@@ -90,6 +106,12 @@ def make_ROIAuto_workflow(my_source_node: pydra.Workflow) -> pydra.Workflow:
 
     roi_workflow.set_output([("outputVolume", roi_workflow.BRAINSROIAuto.lzout.outputVolume)])
     return roi_workflow
+
+def make_LandmarkInitializer_workflow(my_source_node: pydra.Workflow) -> pydra.Workflow:
+    from sem_tasks.utilities.brains import BRAINSLandmarkInitializer
+
+    landmarkInitializer_workflow = pydra.Workflow(name="landmarkInitializer_workflow", input_spec=["t1"], )
+
 
 @pydra.mark.task
 def get_processed_outputs(processed_dict: dict):
@@ -104,14 +126,14 @@ def copy_from_cache(cache_path, output_dir):
     return out_path
 
 # Put the files into the pydra cache and split them into iterable objects. Then pass these iterables into the processing node (preliminary_workflow4)
-source_node = pydra.Workflow(name="source_node", input_spec=["t1_list"])
-source_node.inputs.t1_list = experiment_configuration["t1_list"]
-source_node.split("t1_list")  # Create an iterable for each t1 input file (for preliminary pipeline 3, the input files are .txt)
+source_node = pydra.Workflow(name="source_node", input_spec=["input_data"])
+source_node.inputs.input_data = experiment_configuration["input_data"]
+source_node.split("input_data")  # Create an iterable for each t1 input file (for preliminary pipeline 3, the input files are .txt)
 
 # Get the processing workflow defined in a separate function
 # preliminary_workflow4 = make_bcd_workflow(source_node)
-# preliminary_workflow4 = make_resample_workflow(source_node)
-preliminary_workflow4 = make_ROIAuto_workflow(source_node)
+preliminary_workflow4 = make_resample_workflow(source_node)
+# preliminary_workflow4 = make_ROIAuto_workflow(source_node)
 
 # The sink converts the cached files to output_dir, a location on the local machine
 sink_node = pydra.Workflow(name="sink_node", input_spec=['processed_files'], processed_files=preliminary_workflow4.lzout.all_)
