7f3d8e89ba28d16b530d8c790ef2f956c0217e31
Chase Johnson <chaser.johnson27@gmail.com>
2021-01-26 16:51:01 -0600

    Prepared ROI to be run with the actual application.

--- preliminary_pipeline4.py
+++ preliminary_pipeline4.py
@@ -46,14 +46,14 @@ def make_bcd_workflow(my_source_node: pydra.Workflow) -> pydra.Workflow:
     # Create and fill a task to run a dummy BRAINSConstellationDetector script that runs touch for all the output files
     bcd_task = BRAINSConstellationDetector(name="BRAINSConstellationDetector", executable=experiment_configuration['BRAINSConstellationDetector']['executable']).get_task()
     bcd_task.inputs.inputVolume = bcd_workflow.get_t1.lzout.out
-    bcd_task.inputs.LLSModel = experiment_configuration['BRAINSConstellationDetector']['LLSModel']
-    bcd_task.inputs.acLowerBound = experiment_configuration['BRAINSConstellationDetector']['acLowerBound']
-    bcd_task.inputs.atlasLandmarkWeights = experiment_configuration['BRAINSConstellationDetector']['atlasLandmarkWeights']
-    bcd_task.inputs.atlasLandmarks = experiment_configuration['BRAINSConstellationDetector']['atlasLandmarks']
-    bcd_task.inputs.houghEyeDetectorMode = experiment_configuration['BRAINSConstellationDetector']['houghEyeDetectorMode']
-    # bcd_task.inputs.inputLandmarksEMSP = experiment_configuration['BRAINSConstellationDetector']['inputLandmarksEMSP']
-    bcd_task.inputs.inputTemplateModel = experiment_configuration['BRAINSConstellationDetector']['inputTemplateModel']
-    bcd_task.inputs.interpolationMode = experiment_configuration['BRAINSConstellationDetector']['interpolationMode']
+    bcd_task.inputs.LLSModel = experiment_configuration['BRAINSConstellationDetector'].get('LLSModel')
+    bcd_task.inputs.acLowerBound = experiment_configuration['BRAINSConstellationDetector'].get('acLowerBound')
+    bcd_task.inputs.atlasLandmarkWeights = experiment_configuration['BRAINSConstellationDetector'].get('atlasLandmarkWeights')
+    bcd_task.inputs.atlasLandmarks = experiment_configuration['BRAINSConstellationDetector'].get('atlasLandmarks')
+    bcd_task.inputs.houghEyeDetectorMode = experiment_configuration['BRAINSConstellationDetector'].get('houghEyeDetectorMode')
+    bcd_task.inputs.inputLandmarksEMSP = experiment_configuration['BRAINSConstellationDetector'].get('inputLandmarksEMSP')
+    bcd_task.inputs.inputTemplateModel = experiment_configuration['BRAINSConstellationDetector'].get('inputTemplateModel')
+    bcd_task.inputs.interpolationMode = experiment_configuration['BRAINSConstellationDetector'].get('interpolationMode')
     bcd_task.inputs.outputLandmarksInInputSpace = bcd_workflow.outputLandmarksInInputSpace.lzout.out
     bcd_task.inputs.outputResampledVolume = bcd_workflow.outputResampledVolume.lzout.out
     bcd_task.inputs.outputTransform = bcd_workflow.outputTransform.lzout.out
@@ -86,10 +86,10 @@ def make_resample_workflow(my_source_node: pydra.Workflow) -> pydra.Workflow:
     # Set the inputs of Resample
     resample_task = BRAINSResample("BRAINSResample", executable=experiment_configuration['BRAINSResample']['executable']).get_task()
     resample_task.inputs.inputVolume = resample_workflow.get_t1.lzout.out
-    resample_task.inputs.interpolationMode = experiment_configuration["BRAINSResample"]["interpolationMode"]
-    # resample_task.inputs.pixelType = experiment_configuration["BRAINSResample"]["pixelType"]
-    # resample_task.inputs.referenceVolume = experiment_configuration["BRAINSResample"]["referenceVolume"]
-    resample_task.inputs.warpTransform = experiment_configuration["BRAINSResample"]["warpTransform"]
+    resample_task.inputs.interpolationMode = experiment_configuration["BRAINSResample"].get("interpolationMode")
+    resample_task.inputs.pixelType = experiment_configuration["BRAINSResample"].get("pixelType")
+    resample_task.inputs.referenceVolume = experiment_configuration["BRAINSResample"].get("referenceVolume")
+    resample_task.inputs.warpTransform = experiment_configuration["BRAINSResample"].get("warpTransform")
     resample_task.inputs.outputVolume = resample_workflow.resampledOutputVolume.lzout.out
 
     resample_workflow.add(resample_task)
@@ -102,13 +102,13 @@ def make_ROIAuto_workflow(my_source_node: pydra.Workflow) -> pydra.Workflow:
 
     roi_workflow = pydra.Workflow(name="roi_workflow", input_spec=["input_data"], input_data=my_source_node.lzin.input_data)
     roi_workflow.add(get_input_field(name="get_t1", input_dict=roi_workflow.lzin.input_data, field="t1"))
-    roi_workflow.add(append_filename(name="roiOutputVolume", filename=roi_workflow.get_t1.lzout.out, before_str="Cropped_", append_str="_Aligned", extension=".txt"))
+    roi_workflow.add(append_filename(name="outputVolume", filename=experiment_configuration['BRAINSROIAuto'].get('outputVolume')))
 
-    roi_task = BRAINSROIAuto("BRAINSROIAuto", executable=experiment_configuration['BRAINSROIAuto']['executable']).get_task()
+    roi_task = BRAINSROIAuto("BRAINSROIAuto", executable=experiment_configuration['BRAINSROIAuto'].get('executable')).get_task()
     roi_task.inputs.inputVolume = roi_workflow.get_t1.lzout.out
-    roi_task.inputs.ROIAutoDilateSize = experiment_configuration['BRAINSROIAuto']['ROIAutoDilateSize']
-    roi_task.inputs.cropOutput = experiment_configuration['BRAINSROIAuto']['cropOutput']
-    roi_task.inputs.outputVolume = roi_workflow.roiOutputVolume.lzout.out
+    roi_task.inputs.ROIAutoDilateSize = experiment_configuration['BRAINSROIAuto'].get('ROIAutoDilateSize')
+    roi_task.inputs.cropOutput = experiment_configuration['BRAINSROIAuto'].get('cropOutput')
+    roi_task.inputs.outputVolume = roi_workflow.outputVolume.lzout.out
 
     roi_workflow.add(roi_task)
     roi_workflow.set_output([("outputVolume", roi_workflow.BRAINSROIAuto.lzout.outputVolume)])
@@ -122,10 +122,10 @@ def make_LandmarkInitializer_workflow(my_source_node: pydra.Workflow) -> pydra.W
     landmark_initializer_workflow.add(get_input_field(name="get_movingLandmark", input_dict=landmark_initializer_workflow.lzin.input_data, field="inputMovingLandmarkFilename"))
     landmark_initializer_workflow.add(append_filename(name="outputTransformFilename", filename="landmarkInitializer_subject_to_atlas_transform", extension=".h5"))
 
-    landmark_initializer_task = BRAINSLandmarkInitializer(name="BRAINSLandmarkInitializer", executable=experiment_configuration['BRAINSLandmarkInitializer']['executable']).get_task()
-    landmark_initializer_task.inputs.inputFixedLandmarkFilename = experiment_configuration['BRAINSLandmarkInitializer']['inputFixedLandmarkFilename']
+    landmark_initializer_task = BRAINSLandmarkInitializer(name="BRAINSLandmarkInitializer", executable=experiment_configuration['BRAINSLandmarkInitializer'].get('executable')).get_task()
+    landmark_initializer_task.inputs.inputFixedLandmarkFilename = experiment_configuration['BRAINSLandmarkInitializer'].get('inputFixedLandmarkFilename')
     landmark_initializer_task.inputs.inputMovingLandmarkFilename = landmark_initializer_workflow.get_movingLandmark.lzout.out
-    landmark_initializer_task.inputs.inputWeightFilename = experiment_configuration['BRAINSLandmarkInitializer']['inputWeightFilename']
+    landmark_initializer_task.inputs.inputWeightFilename = experiment_configuration['BRAINSLandmarkInitializer'].get('inputWeightFilename')
     landmark_initializer_task.inputs.outputTransformFilename = landmark_initializer_workflow.outputTransformFilename.lzout.out
 
     landmark_initializer_workflow.add(landmark_initializer_task)
@@ -141,21 +141,21 @@ def make_ABC_workflow(my_source_node: pydra.Workflow) -> pydra.Workflow:
     abc_workflow.add(append_filename(name="outputVolumes", filename=abc_workflow.get_inputVolumes.lzout.out, append_str="_corrected", extension=".txt"))
 
     abc_task = BRAINSABC(name="BRAINSABC", executable=experiment_configuration['BRAINSABC']['executable']).get_task()
-    abc_task.inputs.atlasDefinition = experiment_configuration['BRAINSABC']['atlasDefinition']
-    abc_task.inputs.atlasToSubjectTransform = experiment_configuration['BRAINSABC']['atlasToSubjectTransform']
-    abc_task.inputs.atlasToSubjectTransformType = experiment_configuration['BRAINSABC']['atlasToSubjectTransformType']
-    abc_task.inputs.debuglevel = experiment_configuration['BRAINSABC']['debuglevel']
-    abc_task.inputs.filterIteration = experiment_configuration['BRAINSABC']['filterIteration']
-    abc_task.inputs.filterMethod = experiment_configuration['BRAINSABC']['filterMethod']
-    abc_task.inputs.inputVolumeTypes = experiment_configuration['BRAINSABC']['inputVolumeTypes']
+    abc_task.inputs.atlasDefinition = experiment_configuration['BRAINSABC'].get('atlasDefinition')
+    abc_task.inputs.atlasToSubjectTransform = experiment_configuration['BRAINSABC'].get('atlasToSubjectTransform')
+    abc_task.inputs.atlasToSubjectTransformType = experiment_configuration['BRAINSABC'].get('atlasToSubjectTransformType')
+    abc_task.inputs.debuglevel = experiment_configuration['BRAINSABC'].get('debuglevel')
+    abc_task.inputs.filterIteration = experiment_configuration['BRAINSABC'].get('filterIteration')
+    abc_task.inputs.filterMethod = experiment_configuration['BRAINSABC'].get('filterMethod')
+    abc_task.inputs.inputVolumeTypes = experiment_configuration['BRAINSABC'].get('inputVolumeTypes')
     abc_task.inputs.inputVolumes = abc_workflow.get_inputVolumes.lzout.out
-    abc_task.inputs.interpolationMode = experiment_configuration['BRAINSABC']['interpolationMode']
-    abc_task.inputs.maxBiasDegree = experiment_configuration['BRAINSABC']['maxBiasDegree']
-    abc_task.inputs.maxIterations = experiment_configuration['BRAINSABC']['maxIterations']
-    abc_task.inputs.outputDir = experiment_configuration['BRAINSABC']['outputDir']
-    abc_task.inputs.outputDirtyLabels = experiment_configuration['BRAINSABC']['outputDirtyLabels']
-    abc_task.inputs.outputFormat = experiment_configuration['BRAINSABC']['outputFormat']
-    abc_task.inputs.outputLabels = experiment_configuration['BRAINSABC']['outputLabels']
+    abc_task.inputs.interpolationMode = experiment_configuration['BRAINSABC'].get('interpolationMode')
+    abc_task.inputs.maxBiasDegree = experiment_configuration['BRAINSABC'].get('maxBiasDegree')
+    abc_task.inputs.maxIterations = experiment_configuration['BRAINSABC'].get('maxIterations')
+    abc_task.inputs.outputDir = experiment_configuration['BRAINSABC'].get('outputDir')
+    abc_task.inputs.outputDirtyLabels = experiment_configuration['BRAINSABC'].get('outputDirtyLabels')
+    abc_task.inputs.outputFormat = experiment_configuration['BRAINSABC'].get('outputFormat')
+    abc_task.inputs.outputLabels = experiment_configuration['BRAINSABC'].get('outputLabels')
     abc_task.inputs.outputVolumes = abc_workflow.outputVolumes.lzout.out
 
     abc_workflow.add(abc_task)
@@ -169,12 +169,12 @@ def make_CreateLabelMapFromProbabilityMaps_workflow(my_source_node: pydra.Workfl
     label_map_workflow = pydra.Workflow(name="label_map_workflow", input_spec=["input_data"], input_data=my_source_node.lzin.input_data)
 
     label_map_task = BRAINSCreateLabelMapFromProbabilityMaps(name="BRAINSCreateLabelMapFromProbabilityMaps", executable=experiment_configuration['BRAINSCreateLabelMapFromProbabilityMaps']['executable']).get_task()
-    label_map_task.inputs.cleanLabelVolume = experiment_configuration['BRAINSCreateLabelMapFromProbabilityMaps']['cleanLabelVolume']
-    label_map_task.inputs.dirtyLabelVolume = experiment_configuration['BRAINSCreateLabelMapFromProbabilityMaps']['dirtyLabelVolume']
-    label_map_task.inputs.foregroundPriors = experiment_configuration['BRAINSCreateLabelMapFromProbabilityMaps']['foregroundPriors']
-    label_map_task.inputs.inputProbabilityVolume = experiment_configuration['BRAINSCreateLabelMapFromProbabilityMaps']['inputProbabilityVolume']
-    label_map_task.inputs.nonAirRegionMask = experiment_configuration['BRAINSCreateLabelMapFromProbabilityMaps']['nonAirRegionMask']
-    label_map_task.inputs.priorLabelCodes = experiment_configuration['BRAINSCreateLabelMapFromProbabilityMaps']['priorLabelCodes']
+    label_map_task.inputs.cleanLabelVolume = experiment_configuration['BRAINSCreateLabelMapFromProbabilityMaps'].get('cleanLabelVolume')
+    label_map_task.inputs.dirtyLabelVolume = experiment_configuration['BRAINSCreateLabelMapFromProbabilityMaps'].get('dirtyLabelVolume')
+    label_map_task.inputs.foregroundPriors = experiment_configuration['BRAINSCreateLabelMapFromProbabilityMaps'].get('foregroundPriors')
+    label_map_task.inputs.inputProbabilityVolume = experiment_configuration['BRAINSCreateLabelMapFromProbabilityMaps'].get('inputProbabilityVolume')
+    label_map_task.inputs.nonAirRegionMask = experiment_configuration['BRAINSCreateLabelMapFromProbabilityMaps'].get('nonAirRegionMask')
+    label_map_task.inputs.priorLabelCodes = experiment_configuration['BRAINSCreateLabelMapFromProbabilityMaps'].get('priorLabelCodes')
 
     label_map_workflow.add(label_map_task)
     label_map_workflow.set_output([("cleanLabelVolume", label_map_workflow.BRAINSCreateLabelMapFromProbabilityMaps.lzout.cleanLabelVolume),
@@ -189,24 +189,24 @@ def make_antsRegistration_workflow(my_source_node: pydra.Workflow) -> pydra.Work
     # antsRegistration_workflow.add(append_filename(name="outputVolumes", filename=antsRegistration_workflow.get_output.lzout.out))
 
     antsRegistration_task = ANTSRegistration(name="ANTSRegistration", executable=experiment_configuration['ANTSRegistration']['executable']).get_task()
-    antsRegistration_task.inputs.verbose = experiment_configuration['ANTSRegistration']['verbose']
-    antsRegistration_task.inputs.collapse_output_transforms = experiment_configuration['ANTSRegistration']['collapse-output-transforms']
-    antsRegistration_task.inputs.dimensionality = experiment_configuration['ANTSRegistration']['dimensionality']
-    antsRegistration_task.inputs.float = experiment_configuration['ANTSRegistration']['float']
-    antsRegistration_task.inputs.initial_moving_transform = experiment_configuration['ANTSRegistration']['initial-moving-transform']
-    antsRegistration_task.inputs.initialize_transforms_per_stage = experiment_configuration['ANTSRegistration']['initialize-transforms-per-stage']
-    antsRegistration_task.inputs.interpolation = experiment_configuration['ANTSRegistration']['interpolation']
-    antsRegistration_task.inputs.output = experiment_configuration['ANTSRegistration']['output'] #antsRegistration_workflow.outputVolumes.lzout.out # # # # [ "AtlasToSubjectPreBABC_Rigid", "atlas2subjectRigid.nii.gz", "subject2atlasRigid.nii.gz"] ,
-    antsRegistration_task.inputs.transform = experiment_configuration['ANTSRegistration']['transform']
-    antsRegistration_task.inputs.metric = experiment_configuration['ANTSRegistration']['metric']
-    antsRegistration_task.inputs.convergence = experiment_configuration['ANTSRegistration']['convergence']
-    antsRegistration_task.inputs.smoothing_sigmas = experiment_configuration['ANTSRegistration']['smoothing-sigmas']
-    antsRegistration_task.inputs.shrink_factors = experiment_configuration['ANTSRegistration']['shrink-factors']
-    antsRegistration_task.inputs.use_estimate_learning_rate_once = experiment_configuration['ANTSRegistration']['use-estimate-learning-rate-once']
-    antsRegistration_task.inputs.use_histogram_matching = experiment_configuration['ANTSRegistration']['use-histogram-matching']
-    antsRegistration_task.inputs.masks = experiment_configuration['ANTSRegistration']['masks']
-    antsRegistration_task.inputs.winsorize_image_intensities = experiment_configuration['ANTSRegistration']['winsorize-image-intensities']
-    antsRegistration_task.inputs.write_composite_transform = experiment_configuration['ANTSRegistration']['write-composite-transform']
+    antsRegistration_task.inputs.verbose = experiment_configuration['ANTSRegistration'].get('verbose')
+    antsRegistration_task.inputs.collapse_output_transforms = experiment_configuration['ANTSRegistration'].get('collapse-output-transforms')
+    antsRegistration_task.inputs.dimensionality = experiment_configuration['ANTSRegistration'].get('dimensionality')
+    antsRegistration_task.inputs.float = experiment_configuration['ANTSRegistration'].get('float')
+    antsRegistration_task.inputs.initial_moving_transform = experiment_configuration['ANTSRegistration'].get('initial-moving-transform')
+    antsRegistration_task.inputs.initialize_transforms_per_stage = experiment_configuration['ANTSRegistration'].get('initialize-transforms-per-stage')
+    antsRegistration_task.inputs.interpolation = experiment_configuration['ANTSRegistration'].get('interpolation')
+    antsRegistration_task.inputs.output = experiment_configuration['ANTSRegistration'].get('output') #antsRegistration_workflow.outputVolumes.lzout.out # # # # [ "AtlasToSubjectPreBABC_Rigid", "atlas2subjectRigid.nii.gz", "subject2atlasRigid.nii.gz"] ,
+    antsRegistration_task.inputs.transform = experiment_configuration['ANTSRegistration'].get('transform')
+    antsRegistration_task.inputs.metric = experiment_configuration['ANTSRegistration'].get('metric')
+    antsRegistration_task.inputs.convergence = experiment_configuration['ANTSRegistration'].get('convergence')
+    antsRegistration_task.inputs.smoothing_sigmas = experiment_configuration['ANTSRegistration'].get('smoothing-sigmas')
+    antsRegistration_task.inputs.shrink_factors = experiment_configuration['ANTSRegistration'].get('shrink-factors')
+    antsRegistration_task.inputs.use_estimate_learning_rate_once = experiment_configuration['ANTSRegistration'].get('use-estimate-learning-rate-once')
+    antsRegistration_task.inputs.use_histogram_matching = experiment_configuration['ANTSRegistration'].get('use-histogram-matching')
+    antsRegistration_task.inputs.masks = experiment_configuration['ANTSRegistration'].get('masks')
+    antsRegistration_task.inputs.winsorize_image_intensities = experiment_configuration['ANTSRegistration'].get('winsorize-image-intensities')
+    antsRegistration_task.inputs.write_composite_transform = experiment_configuration['ANTSRegistration'].get('write-composite-transform')
 
     antsRegistration_workflow.add(antsRegistration_task)
     antsRegistration_workflow.set_output([("output", antsRegistration_workflow.ANTSRegistration.lzout.output)])
@@ -242,8 +242,8 @@ source_node.split("input_data")  # Create an iterable for each t1 input file (fo
 
 # Get the processing workflow defined in a separate function
 # preliminary_workflow4 = make_bcd_workflow(source_node)
-preliminary_workflow4 = make_resample_workflow(source_node)
-# preliminary_workflow4 = make_ROIAuto_workflow(source_node)
+# preliminary_workflow4 = make_resample_workflow(source_node)
+preliminary_workflow4 = make_ROIAuto_workflow(source_node)
 # preliminary_workflow4 = make_LandmarkInitializer_workflow(source_node)
 # preliminary_workflow4 = make_ABC_workflow(source_node)
 # preliminary_workflow4 = make_CreateLabelMapFromProbabilityMaps_workflow(source_node)
