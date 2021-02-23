91ebfd5efaca65848571c59395ad61bac26522b8
Chase Johnson <chaser.johnson27@gmail.com>
2021-02-23 09:51:52 -0600

    Preparing to run antsRegistration2.

--- preliminary_pipeline5.py
+++ preliminary_pipeline5.py
@@ -430,7 +430,7 @@ processing_node.add(make_landmarkInitializer_workflow2(inputFixedLandmarkFilenam
 processing_node.add(make_resample_workflow1(inputVolume=processing_node.inputs_workflow.lzout.inputVolume, warpTransform=processing_node.landmarkInitializer_workflow1.lzout.outputTransformFilename))
 processing_node.add(make_roi_workflow2(inputVolume=processing_node.roi_workflow1.lzout.outputVolume))
 processing_node.add(make_antsRegistration_workflow1(fixed_image=processing_node.roi_workflow1.lzout.outputVolume, fixed_image_masks=processing_node.roi_workflow2.lzout.outputROIMaskVolume, initial_moving_transform=processing_node.landmarkInitializer_workflow2.lzout.outputTransformFilename))
-# processing_node.add(make_antsRegistration_workflow2(fixed_image=processing_node.roi_workflow1.lzout.outputVolume, fixed_image_masks=processing_node.roi_workflow2.lzout.outputROIMaskVolume, initial_moving_transform=processing_node.antsRegistration_workflow1.lzout.composite_transform))
+processing_node.add(make_antsRegistration_workflow2(fixed_image=processing_node.roi_workflow1.lzout.outputVolume, fixed_image_masks=processing_node.roi_workflow2.lzout.outputROIMaskVolume, initial_moving_transform=processing_node.antsRegistration_workflow1.lzout.composite_transform))
 # processing_node.add(make_abc_workflow1(inputVolumes=processing_node.roi_workflow1.lzout.outputVolume, inputT1=processing_node.inputs_workflow.lzout.inputVolume))
 
 
@@ -442,7 +442,7 @@ processing_node.add(make_antsRegistration_workflow1(fixed_image=processing_node.
 # preliminary_workflow4 = make_antsRegistration_workflow(source_node)
 # preliminary_workflow4 = make_antsRegistration_workflow2(source_node)
 # final_processing_workflow = ants_workflow2
-processing_node.set_output([("out", processing_node.antsRegistration_workflow1.lzout.all_)])
+processing_node.set_output([("out", processing_node.antsRegistration_workflow2.lzout.all_)])
 
 
 # The sink converts the cached files to output_dir, a location on the local machine
