b5c00d254ab0575abd9f45dc8599ac222e6b2335
Chase Johnson <chaser.johnson27@gmail.com>
2021-03-02 08:22:52 -0600

    Added implicitOuptuts again.

--- sem_tasks/segmentation/specialized.py
+++ sem_tasks/segmentation/specialized.py
@@ -316,16 +316,16 @@ class BRAINSABC:
                     metadata={"argstr": "--defaultSuffix ", "help_string": ""},
                 ),
             ),
-            # (
-            #     "implicitOutputs",
-            #     attr.ib(
-            #         type=File,
-            #         metadata={
-            #             "argstr": "--implicitOutputs ",
-            #             "help_string": "Outputs to be made available to NiPype. Needed because not all BRAINSABC outputs have command line arguments.",
-            #         },
-            #     ),
-            # ),
+            (
+                "implicitOutputs",
+                attr.ib(
+                    type=File,
+                    metadata={
+                        "argstr": "--implicitOutputs ",
+                        "help_string": "Outputs to be made available to NiPype. Needed because not all BRAINSABC outputs have command line arguments.",
+                    },
+                ),
+            ),
             (
                 "debuglevel",
                 attr.ib(
