52e8ebed10f2b12993e7c4265dc39290eec7be21
Chase Johnson <chaser.johnson27@gmail.com>
2021-03-05 15:31:42 -0600

    Prepared to run antsRegistration3.

--- preliminary_pipeline5.py
+++ preliminary_pipeline5.py
@@ -653,45 +653,61 @@ def make_antsRegistration_workflow3(fixed_image, fixed_image_masks, initial_movi
     antsRegistration_workflow = pydra.Workflow(name=workflow_name, input_spec=["fixed_image", "fixed_image_masks", "initial_moving_transform"], fixed_image=fixed_image, fixed_image_masks=fixed_image_masks, initial_moving_transform=initial_moving_transform)
 
     antsRegistration_task = Nipype1Task(Registration())
+    # antsRegistration_task.inputs.fixed_image = "/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/t1_average_BRAINSABC.nii.gz"  # antsRegistration_workflow.lzin.fixed_image
+    # antsRegistration_task.inputs.fixed_image_masks = "/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/fixedImageROIAutoMask.nii.gz"  # antsRegistration_workflow.lzin.fixed_image_masks
+    # antsRegistration_task.inputs.initial_moving_transform = "/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/landmarkInitializer_91300_to_subject_transform.h5"  # antsRegistration_workflow.lzin.initial_moving_transform
 
-    # Set subject-specific files
-    antsRegistration_task.inputs.fixed_image =                      "/localscratch/Users/cjohnson30/output_dir/sub-052823_ses-43817_run-002_T1w/t1_average_BRAINSABC.nii.gz" #antsRegistration_workflow.lzin.fixed_image
-    antsRegistration_task.inputs.fixed_image_masks =                "/localscratch/Users/cjohnson30/output_dir/sub-052823_ses-43817_run-002_T1w/fixedImageROIAutoMask.nii.gz" #antsRegistration_workflow.lzin.fixed_image_masks
-    antsRegistration_task.inputs.initial_moving_transform =         "/localscratch/Users/cjohnson30/output_dir/sub-052823_ses-43817_run-002_T1w/landmarkInitializer_91300_to_subject_transform.h5" #antsRegistration_workflow.lzin.initial_moving_transform
+    antsRegistration_task.inputs.fixed_image =                      antsRegistration_workflow.lzin.fixed_image
+    antsRegistration_task.inputs.fixed_image_masks =                antsRegistration_workflow.lzin.fixed_image_masks
+    antsRegistration_task.inputs.initial_moving_transform =         antsRegistration_workflow.lzin.initial_moving_transform
 
-    antsRegistration_task.inputs.moving_image =                     experiment_configuration[configkey].get('moving_image')
-    antsRegistration_task.inputs.moving_image_masks =               experiment_configuration[configkey].get('moving_image_masks')
-    antsRegistration_task.inputs.save_state =                       experiment_configuration[configkey].get('save_state')
-    antsRegistration_task.inputs.transforms =                       experiment_configuration[configkey].get('transforms')
-    antsRegistration_task.inputs.transform_parameters =             experiment_configuration[configkey].get('transform_parameters')
-    antsRegistration_task.inputs.number_of_iterations =             experiment_configuration[configkey].get('number_of_iterations')
-    antsRegistration_task.inputs.dimension =                        experiment_configuration[configkey].get('dimensionality')
-    antsRegistration_task.inputs.write_composite_transform =        experiment_configuration[configkey].get('write_composite_transform')
-    antsRegistration_task.inputs.collapse_output_transforms =       experiment_configuration[configkey].get('collapse_output_transforms')
-    antsRegistration_task.inputs.verbose =                          experiment_configuration[configkey].get('verbose')
-    antsRegistration_task.inputs.initialize_transforms_per_stage =  experiment_configuration[configkey].get('initialize_transforms_per_stage')
-    antsRegistration_task.inputs.float =                            experiment_configuration[configkey].get('float')
-    antsRegistration_task.inputs.metric =                           experiment_configuration[configkey].get('metric')
-    antsRegistration_task.inputs.metric_weight =                    experiment_configuration[configkey].get('metric_weight')
-    antsRegistration_task.inputs.radius_or_number_of_bins =         experiment_configuration[configkey].get('radius_or_number_of_bins')
-    antsRegistration_task.inputs.sampling_strategy =                experiment_configuration[configkey].get('sampling_strategy')
-    antsRegistration_task.inputs.sampling_percentage =              experiment_configuration[configkey].get('sampling_percentage')
-    antsRegistration_task.inputs.convergence_threshold =            experiment_configuration[configkey].get('convergence_threshold')
-    antsRegistration_task.inputs.convergence_window_size =          experiment_configuration[configkey].get('convergence_window_size')
-    antsRegistration_task.inputs.smoothing_sigmas =                 experiment_configuration[configkey].get('smoothing_sigmas')
-    antsRegistration_task.inputs.sigma_units =                      experiment_configuration[configkey].get('sigma_units')
-    antsRegistration_task.inputs.shrink_factors =                   experiment_configuration[configkey].get('shrink_factors')
-    antsRegistration_task.inputs.use_estimate_learning_rate_once =  experiment_configuration[configkey].get('use_estimate_learning_rate_once')
-    antsRegistration_task.inputs.use_histogram_matching =           experiment_configuration[configkey].get('use_histogram_matching')
-    antsRegistration_task.inputs.winsorize_lower_quantile =         experiment_configuration[configkey].get('winsorize_lower_quantile')
-    antsRegistration_task.inputs.winsorize_upper_quantile =         experiment_configuration[configkey].get('winsorize_upper_quantile')
+
+
+    antsRegistration_task.inputs.moving_image = experiment_configuration[configkey].get('moving_image')
+    antsRegistration_task.inputs.moving_image_masks = experiment_configuration[configkey].get('moving_image_masks')
+    antsRegistration_task.inputs.save_state = experiment_configuration[configkey].get('save_state')
+    antsRegistration_task.inputs.transforms = experiment_configuration[configkey].get('transforms')
+    antsRegistration_task.inputs.transform_parameters = experiment_configuration[configkey].get('transform_parameters')
+    antsRegistration_task.inputs.number_of_iterations = experiment_configuration[configkey].get('number_of_iterations')
+    antsRegistration_task.inputs.dimension = experiment_configuration[configkey].get('dimensionality')
+    antsRegistration_task.inputs.write_composite_transform = experiment_configuration[configkey].get(
+        'write_composite_transform')
+    antsRegistration_task.inputs.collapse_output_transforms = experiment_configuration[configkey].get(
+        'collapse_output_transforms')
+    antsRegistration_task.inputs.verbose = experiment_configuration[configkey].get('verbose')
+    antsRegistration_task.inputs.initialize_transforms_per_stage = experiment_configuration[configkey].get(
+        'initialize_transforms_per_stage')
+    antsRegistration_task.inputs.float = experiment_configuration[configkey].get('float')
+    antsRegistration_task.inputs.metric = experiment_configuration[configkey].get('metric')
+    antsRegistration_task.inputs.metric_weight = experiment_configuration[configkey].get('metric_weight')
+    antsRegistration_task.inputs.radius_or_number_of_bins = experiment_configuration[configkey].get(
+        'radius_or_number_of_bins')
+    antsRegistration_task.inputs.sampling_strategy = experiment_configuration[configkey].get('sampling_strategy')
+    antsRegistration_task.inputs.sampling_percentage = experiment_configuration[configkey].get('sampling_percentage')
+    antsRegistration_task.inputs.convergence_threshold = experiment_configuration[configkey].get(
+        'convergence_threshold')
+    antsRegistration_task.inputs.convergence_window_size = experiment_configuration[configkey].get(
+        'convergence_window_size')
+    antsRegistration_task.inputs.smoothing_sigmas = experiment_configuration[configkey].get('smoothing_sigmas')
+    antsRegistration_task.inputs.sigma_units = experiment_configuration[configkey].get('sigma_units')
+    antsRegistration_task.inputs.shrink_factors = experiment_configuration[configkey].get('shrink_factors')
+    antsRegistration_task.inputs.use_estimate_learning_rate_once = experiment_configuration[configkey].get(
+        'use_estimate_learning_rate_once')
+    antsRegistration_task.inputs.use_histogram_matching = experiment_configuration[configkey].get(
+        'use_histogram_matching')
+    antsRegistration_task.inputs.winsorize_lower_quantile = experiment_configuration[configkey].get(
+        'winsorize_lower_quantile')
+    antsRegistration_task.inputs.winsorize_upper_quantile = experiment_configuration[configkey].get(
+        'winsorize_upper_quantile')
 
     # Set the variables that set output file names
-    antsRegistration_task.inputs.output_transform_prefix =          experiment_configuration[configkey].get('output_transform_prefix')
-    antsRegistration_task.inputs.output_warped_image =              experiment_configuration[configkey].get('output_warped_image')
-    antsRegistration_task.inputs.output_inverse_warped_image =      experiment_configuration[configkey].get('output_inverse_warped_image')
-
+    antsRegistration_task.inputs.output_transform_prefix = experiment_configuration[configkey].get(
+        'output_transform_prefix')
+    antsRegistration_task.inputs.output_warped_image = experiment_configuration[configkey].get('output_warped_image')
+    antsRegistration_task.inputs.output_inverse_warped_image = experiment_configuration[configkey].get(
+        'output_inverse_warped_image')
     # print(antsRegistration_task.cmdline)
+
     antsRegistration_workflow.add(antsRegistration_task)
     antsRegistration_workflow.set_output([
         ("save_state", antsRegistration_task.lzout.save_state),
@@ -742,25 +758,25 @@ processing_node = pydra.Workflow(name="processing_node", input_spec=["input_data
 processing_node.add(get_inputs_workflow(my_source_node=processing_node))
 
 
-# processing_node.add(make_bcd_workflow1(inputVolume=processing_node.inputs_workflow.lzout.inputVolume, inputLandmarksEMSP=processing_node.inputs_workflow.lzout.inputLandmarksEMSP))
-# processing_node.add(make_roi_workflow1(inputVolume=processing_node.bcd_workflow1.lzout.outputResampledVolume))
-# processing_node.add(make_landmarkInitializer_workflow1(inputMovingLandmarkFilename=processing_node.bcd_workflow1.lzout.outputLandmarksInInputSpace))
-# processing_node.add(make_landmarkInitializer_workflow2(inputFixedLandmarkFilename=processing_node.bcd_workflow1.lzout.outputLandmarksInACPCAlignedSpace))
-# processing_node.add(make_resample_workflow1(inputVolume=processing_node.inputs_workflow.lzout.inputVolume, warpTransform=processing_node.landmarkInitializer_workflow1.lzout.outputTransformFilename))
-# processing_node.add(make_roi_workflow2(inputVolume=processing_node.roi_workflow1.lzout.outputVolume))
-# processing_node.add(make_antsRegistration_workflow1(fixed_image=processing_node.roi_workflow1.lzout.outputVolume, fixed_image_masks=processing_node.roi_workflow2.lzout.outputROIMaskVolume, initial_moving_transform=processing_node.landmarkInitializer_workflow2.lzout.outputTransformFilename))
-# processing_node.add(make_antsRegistration_workflow2(fixed_image=processing_node.roi_workflow1.lzout.outputVolume, fixed_image_masks=processing_node.roi_workflow2.lzout.outputROIMaskVolume, initial_moving_transform=processing_node.antsRegistration_workflow1.lzout.composite_transform))
-# processing_node.add(make_abc_workflow1(inputVolumes=processing_node.roi_workflow1.lzout.outputVolume, inputT1=processing_node.inputs_workflow.lzout.inputVolume, restoreState=processing_node.antsRegistration_workflow2.lzout.save_state))
-# processing_node.add(make_resample_workflow2(referenceVolume=processing_node.abc_workflow1.lzout.t1_average, warpTransform=processing_node.abc_workflow1.lzout.atlasToSubjectTransform))
-# processing_node.add(make_resample_workflow3(referenceVolume=processing_node.abc_workflow1.lzout.t1_average, warpTransform=processing_node.abc_workflow1.lzout.atlasToSubjectTransform))
-# processing_node.add(make_resample_workflow4(referenceVolume=processing_node.abc_workflow1.lzout.t1_average, warpTransform=processing_node.abc_workflow1.lzout.atlasToSubjectTransform))
-# processing_node.add(make_resample_workflow5(referenceVolume=processing_node.abc_workflow1.lzout.t1_average, warpTransform=processing_node.abc_workflow1.lzout.atlasToSubjectTransform))
-# processing_node.add(make_resample_workflow6(referenceVolume=processing_node.abc_workflow1.lzout.t1_average, warpTransform=processing_node.abc_workflow1.lzout.atlasToSubjectTransform))
-# processing_node.add(make_resample_workflow7(referenceVolume=processing_node.abc_workflow1.lzout.t1_average, warpTransform=processing_node.abc_workflow1.lzout.atlasToSubjectTransform))
-# processing_node.add(make_resample_workflow8(referenceVolume=processing_node.abc_workflow1.lzout.t1_average, warpTransform=processing_node.abc_workflow1.lzout.atlasToSubjectTransform))
-# processing_node.add(make_createLabelMapFromProbabilityMaps_workflow1(inputProbabilityVolume=processing_node.abc_workflow1.lzout.posteriors, nonAirRegionMask=processing_node.roi_workflow2.lzout.outputROIMaskVolume))
-# processing_node.add(make_landmarkInitializer_workflow3(inputMovingLandmarkFilename=experiment_configuration["BRAINSLandmarkInitializer23"].get('inputMovingLandmarkFilename'), inputFixedLandmarkFilename=processing_node.bcd_workflow1.lzout.outputLandmarksInACPCAlignedSpace).split("inputMovingLandmarkFilename"))
-processing_node.add(make_antsRegistration_workflow3(fixed_image=None, fixed_image_masks=None, initial_moving_transform=None))
+processing_node.add(make_bcd_workflow1(inputVolume=processing_node.inputs_workflow.lzout.inputVolume, inputLandmarksEMSP=processing_node.inputs_workflow.lzout.inputLandmarksEMSP))
+processing_node.add(make_roi_workflow1(inputVolume=processing_node.bcd_workflow1.lzout.outputResampledVolume))
+processing_node.add(make_landmarkInitializer_workflow1(inputMovingLandmarkFilename=processing_node.bcd_workflow1.lzout.outputLandmarksInInputSpace))
+processing_node.add(make_landmarkInitializer_workflow2(inputFixedLandmarkFilename=processing_node.bcd_workflow1.lzout.outputLandmarksInACPCAlignedSpace))
+processing_node.add(make_resample_workflow1(inputVolume=processing_node.inputs_workflow.lzout.inputVolume, warpTransform=processing_node.landmarkInitializer_workflow1.lzout.outputTransformFilename))
+processing_node.add(make_roi_workflow2(inputVolume=processing_node.roi_workflow1.lzout.outputVolume))
+processing_node.add(make_antsRegistration_workflow1(fixed_image=processing_node.roi_workflow1.lzout.outputVolume, fixed_image_masks=processing_node.roi_workflow2.lzout.outputROIMaskVolume, initial_moving_transform=processing_node.landmarkInitializer_workflow2.lzout.outputTransformFilename))
+processing_node.add(make_antsRegistration_workflow2(fixed_image=processing_node.roi_workflow1.lzout.outputVolume, fixed_image_masks=processing_node.roi_workflow2.lzout.outputROIMaskVolume, initial_moving_transform=processing_node.antsRegistration_workflow1.lzout.composite_transform))
+processing_node.add(make_abc_workflow1(inputVolumes=processing_node.roi_workflow1.lzout.outputVolume, inputT1=processing_node.inputs_workflow.lzout.inputVolume, restoreState=processing_node.antsRegistration_workflow2.lzout.save_state))
+processing_node.add(make_resample_workflow2(referenceVolume=processing_node.abc_workflow1.lzout.t1_average, warpTransform=processing_node.abc_workflow1.lzout.atlasToSubjectTransform))
+processing_node.add(make_resample_workflow3(referenceVolume=processing_node.abc_workflow1.lzout.t1_average, warpTransform=processing_node.abc_workflow1.lzout.atlasToSubjectTransform))
+processing_node.add(make_resample_workflow4(referenceVolume=processing_node.abc_workflow1.lzout.t1_average, warpTransform=processing_node.abc_workflow1.lzout.atlasToSubjectTransform))
+processing_node.add(make_resample_workflow5(referenceVolume=processing_node.abc_workflow1.lzout.t1_average, warpTransform=processing_node.abc_workflow1.lzout.atlasToSubjectTransform))
+processing_node.add(make_resample_workflow6(referenceVolume=processing_node.abc_workflow1.lzout.t1_average, warpTransform=processing_node.abc_workflow1.lzout.atlasToSubjectTransform))
+processing_node.add(make_resample_workflow7(referenceVolume=processing_node.abc_workflow1.lzout.t1_average, warpTransform=processing_node.abc_workflow1.lzout.atlasToSubjectTransform))
+processing_node.add(make_resample_workflow8(referenceVolume=processing_node.abc_workflow1.lzout.t1_average, warpTransform=processing_node.abc_workflow1.lzout.atlasToSubjectTransform))
+processing_node.add(make_createLabelMapFromProbabilityMaps_workflow1(inputProbabilityVolume=processing_node.abc_workflow1.lzout.posteriors, nonAirRegionMask=processing_node.roi_workflow2.lzout.outputROIMaskVolume))
+processing_node.add(make_landmarkInitializer_workflow3(inputMovingLandmarkFilename=experiment_configuration["BRAINSLandmarkInitializer23"].get('inputMovingLandmarkFilename'), inputFixedLandmarkFilename=processing_node.bcd_workflow1.lzout.outputLandmarksInACPCAlignedSpace).split("inputMovingLandmarkFilename"))
+processing_node.add(make_antsRegistration_workflow3(fixed_image=processing_node.abc_workflow1.lzout.t1_average, fixed_image_masks=processing_node.roi_workflow2.lzout.outputROIMaskVolume, initial_moving_transform="/localscratch/Users/cjohnson30/output_dir/sub-052823_ses-43817_run-002_T1w/landmarkInitializer_91300_to_subject_transform.h5"))
 
 
 processing_node.set_output([
