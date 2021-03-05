d52baf2d5da3e7d7e1e63d369561e7bd447621da
Chase Johnson <chaser.johnson27@gmail.com>
2021-03-05 08:20:57 -0600

    Development debugging scripts.

--- dummy_shell_scripts/test.sh
+++ dummy_shell_scripts/test.sh
@@ -4,7 +4,7 @@
 outputVolume=unset
 
 # Set which arguments will be considered valid arguments
-PARSED_ARGUMENTS=$(getopt -n alphabet -o v:: --long verbose:,collapse-output-transforms:,dimensionality:,float:,initial-moving-transform:,initialize-transforms-per-stage:,interpolation:,output:,transform:,metric:,convergence:,smoothing-sigmas:,shrink-factors:,use-estimate-learning-rate-once:,use-histogram-matching:,masks:,winsorize-image-intensities:,write-composite-transform: -- "$@")
+PARSED_ARGUMENTS=$(getopt -n alphabet -o v:: --long default:,createFiles:,input:,contents:,inputs:,verbose:,collapse-output-transforms:,dimensionality:,float:,initial-moving-transform:,initialize-transforms-per-stage:,interpolation:,output:,transform:,metric:,convergence:,smoothing-sigmas:,shrink-factors:,use-estimate-learning-rate-once:,use-histogram-matching:,masks:,winsorize-image-intensities:,write-composite-transform: -- "$@")
 
 
 #PARSED_ARGUMENTS=$(getopt -n alphabet -o v:: --long : output: -- "$@")
@@ -14,15 +14,25 @@ eval set -- "$PARSED_ARGUMENTS"
 while :
 do
     case "$1" in
+    --output) output="$2" ; shift 2;;
+    --default) default="$2" ; shift 2;;
     --verbose) verbose="$2" ; shift 2 ;;
     --dimensionality) dimensionality="$2" ; shift 2 ;;
     --float) float="$2" ; shift 2 ;;
     --initial-moving-transform) initial_moving_transform="$2" ; shift 2 ;;
     --initialize-transforms-per-stage) initialize-transforms-per-stage="$2" ; shift 2 ;;
     --interpolation) interpolation="$2" ; shift 2 ;;
-    --output) set -f
+    --createFiles) set -f
               IFS=','
-              outputs=($2)
+              createFiles=($2)
+              shift 2 ;;
+    --inputs) set -f
+              IFS=','
+              input=($2)
+              shift 2 ;;
+    --contents) set -f
+              IFS=','
+              contents=($2)
               shift 2 ;;
     --transform) transform="$2" ; shift 2 ;;
     --metric) metric="$2" ; shift 2 ;;
@@ -45,10 +55,14 @@ do
 ## Append "resampled" to outputVolume
 #echo "ants_registration" >> "$output"
 
-
-for i in "${outputs[@]}"; do
+for i in "${createFiles[@]}"; do
   echo "touching ${i}"
   touch "${i}"
-  echo "ants_registration" >> "${i}"
+done
+
+touch "$output"
+for i in "${contents[@]}"; do
+  echo "${i}" >> "$output"
+#  echo contents >> "${i}"
 done
 #for var in "$@"; do touch file"$var".txt; done
