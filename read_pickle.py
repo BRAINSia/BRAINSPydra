6e6aa2a804e5fe81842c08cd487299b1df1274f1
Chase Johnson <chaser.johnson27@gmail.com>
2021-03-09 21:30:31 -0600

    Adding prints to the read_pickle script.

--- read_pickle.py
+++ read_pickle.py
@@ -7,12 +7,16 @@ print(path)
 f=open(path,'rb')
 data=pickle.load(f)
 f.close()
-f=open(sys.argv[2], "a")
 try:
+    f = open(sys.argv[2], "a")
+    print(str(data))
+    print(str(data.get_output_field("stdout")))
+    print(str(data.get_output_field("stderr")))
     f.write(str(data.get_output_field("stdout")))
 except:
     pass
 try:
+    f = open(sys.argv[2], "a")
     f.write(str(data.get_output_field("stderr")))
 except:
     pass
