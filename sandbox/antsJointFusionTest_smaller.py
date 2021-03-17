42892969fd894c3ce4ea1cf154c4828607d94ea6
Chase Johnson <chaser.johnson27@gmail.com>
2021-03-17 14:56:44 -0500

    Trying out lzin with a wrapped interface, but I want to make sure the normal task works before I move on.

--- sandbox/antsJointFusionTest_smaller.py
+++ sandbox/antsJointFusionTest_smaller.py
@@ -2,9 +2,16 @@ from nipype.interfaces.ants import AntsJointFusion
 from pydra.tasks.nipype1.utils import Nipype1Task
 import pydra
 
-antsJointFusion = AntsJointFusion()
-antsJointFusion.num_threads = 28
-antsJointFusion_task = Nipype1Task(antsJointFusion)
+wf = pydra.Workflow(name="wf", input_spec=["atlas_image"], atlas_image=[
+    # ['/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/91300_2subject.nii.gz'],
+    # ['/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/91626_2subject.nii.gz']
+    ['/localscratch/Users/cjohnson30/output_dir/sub-052823_ses-43817_run-002_T1w/91300_2subject.nii.gz'],
+    ['/localscratch/Users/cjohnson30/output_dir/sub-052823_ses-43817_run-002_T1w/91626_2subject.nii.gz'],
+])
+
+# antsJointFusion_task = Nipype1Task(AntsJointFusion())
+antsJointFusion_task = AntsJointFusion()
+# wf.add(antsJointFusion_task)
 antsJointFusion_task.inputs.atlas_image = [
     # ['/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/91300_2subject.nii.gz'],
     # ['/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/91626_2subject.nii.gz']
@@ -27,14 +34,14 @@ antsJointFusion_task.inputs.mask_image = "/localscratch/Users/cjohnson30/output_
 # antsJointFusion_task.inputs.mask_image = "/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/fixedImageROIAutoMask.nii.gz"
 antsJointFusion_task.inputs.out_label_fusion = "JointFusion_HDAtlas20_2015_label.nii.gz"
 antsJointFusion_task.inputs.verbose = True
-# antsJointFusion_task.inputs.num_threads = 28
+antsJointFusion_task.inputs.num_threads = 28
 
-# antsJointFusion_workflow = Nipype1Task(antsJointFusion_task)
+antsJointFusion_workflow = Nipype1Task(antsJointFusion_task)
 
 # print(antsJointFusion_task.cmdline)
 # result = antsJointFusion_task.run()
 # print(result)
 with pydra.Submitter(plugin="cf") as sub:
-    sub(antsJointFusion_task)
-result = antsJointFusion_task.result()
+    sub(antsJointFusion_workflow)
+result = antsJointFusion_workflow.result()
 print(result)
