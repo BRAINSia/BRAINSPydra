9caac961987b3c8d5302f67dbdb5d63dbd648701
Chase Johnson <chaser.johnson27@gmail.com>
2021-03-17 13:35:29 -0500

    Running antsJointFusion using a nipype interface with (hopefully) 28 threads.

--- sandbox/antsJointFusionTest_smaller.py
+++ sandbox/antsJointFusionTest_smaller.py
@@ -18,15 +18,19 @@ antsJointFusion_task.inputs.atlas_segmentation_image = [
 antsJointFusion_task.inputs.beta = 2.0
 antsJointFusion_task.inputs.dimension = 3
 antsJointFusion_task.inputs.search_radius = [3]
-# antsJointFusion_task.inputs.target_image = ['/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/t1_average_BRAINSABC.nii.gz']
 antsJointFusion_task.inputs.target_image = ['/localscratch/Users/cjohnson30/output_dir/sub-052823_ses-43817_run-002_T1w/t1_average_BRAINSABC.nii.gz']
 antsJointFusion_task.inputs.mask_image = "/localscratch/Users/cjohnson30/output_dir/sub-052823_ses-43817_run-002_T1w/fixedImageROIAutoMask.nii.gz"
+# antsJointFusion_task.inputs.target_image = ['/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/t1_average_BRAINSABC.nii.gz']
+# antsJointFusion_task.inputs.mask_image = "/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/fixedImageROIAutoMask.nii.gz"
 antsJointFusion_task.inputs.out_label_fusion = "JointFusion_HDAtlas20_2015_label.nii.gz"
 antsJointFusion_task.inputs.verbose = True
-print(antsJointFusion_task.input_spec)
+antsJointFusion_task.set_default_num_threads(28)
+antsJointFusion_task.inputs.num_threads = 28
+
+# print(antsJointFusion_task.input_spec)
 # antsJointFusion_task.inputs.num_threads(56)
 # antsJointFusion_task.set_default_num_threads(24)
 
 # print(antsJointFusion_task.cmdline)
-# result = antsJointFusion_task.run()
-# print(result)
+result = antsJointFusion_task.run()
+print(result)
