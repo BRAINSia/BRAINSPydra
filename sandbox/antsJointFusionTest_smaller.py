3c11d3059c6d097f1e4cf9e049aa703fae5351da
Chase Johnson <chaser.johnson27@gmail.com>
2021-03-17 15:34:15 -0500

    Just using the ants interface.

--- sandbox/antsJointFusionTest_smaller.py
+++ sandbox/antsJointFusionTest_smaller.py
@@ -2,25 +2,12 @@ from nipype.interfaces.ants import AntsJointFusion
 from pydra.tasks.nipype1.utils import Nipype1Task
 import pydra
 
-# wf = pydra.Workflow(name="wf", input_spec=["atlas_image"], atlas_image=[
-#     # ['/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/91300_2subject.nii.gz'],
-#     # ['/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/91626_2subject.nii.gz']
-#     ['/localscratch/Users/cjohnson30/output_dir/sub-052823_ses-43817_run-002_T1w/91300_2subject.nii.gz'],
-#     ['/localscratch/Users/cjohnson30/output_dir/sub-052823_ses-43817_run-002_T1w/91626_2subject.nii.gz'],
-# ])
-
 antsJointFusion_task = AntsJointFusion()
-# antsJointFusion_task = AntsJointFusion()
-# wf.add(antsJointFusion_task)
 antsJointFusion_task.inputs.atlas_image =[
-    # ['/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/91300_2subject.nii.gz'],
-    # ['/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/91626_2subject.nii.gz']
     ['/localscratch/Users/cjohnson30/output_dir/sub-052823_ses-43817_run-002_T1w/91300_2subject.nii.gz'],
     ['/localscratch/Users/cjohnson30/output_dir/sub-052823_ses-43817_run-002_T1w/91626_2subject.nii.gz'],
 ]
 antsJointFusion_task.inputs.atlas_segmentation_image = [
-    # "/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/91300_2_subj_lbl.nii.gz",
-    # "/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/91626_2_subj_lbl.nii.gz"
     "/localscratch/Users/cjohnson30/output_dir/sub-052823_ses-43817_run-002_T1w/91300_2_subj_lbl.nii.gz",
     "/localscratch/Users/cjohnson30/output_dir/sub-052823_ses-43817_run-002_T1w/91626_2_subj_lbl.nii.gz",
 ]
@@ -30,13 +17,9 @@ antsJointFusion_task.inputs.dimension = 3
 antsJointFusion_task.inputs.search_radius = [3]
 antsJointFusion_task.inputs.target_image = ['/localscratch/Users/cjohnson30/output_dir/sub-052823_ses-43817_run-002_T1w/t1_average_BRAINSABC.nii.gz']
 antsJointFusion_task.inputs.mask_image = "/localscratch/Users/cjohnson30/output_dir/sub-052823_ses-43817_run-002_T1w/fixedImageROIAutoMask.nii.gz"
-# antsJointFusion_task.inputs.target_image = ['/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/t1_average_BRAINSABC.nii.gz']
-# antsJointFusion_task.inputs.mask_image = "/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/fixedImageROIAutoMask.nii.gz"
 antsJointFusion_task.inputs.out_label_fusion = "JointFusion_HDAtlas20_2015_label.nii.gz"
 antsJointFusion_task.inputs.verbose = True
 antsJointFusion_task.inputs.num_threads = 28
-
-antsJointFusion_workflow = Nipype1Task(antsJointFusion_task)
 
 print(antsJointFusion_task.cmdline)
 result = antsJointFusion_task.run()
