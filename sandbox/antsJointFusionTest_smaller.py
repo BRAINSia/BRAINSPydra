bbe6b3a5ad4b3966b25d0355fb8d63910898537f
Chase Johnson <chaser.johnson27@gmail.com>
2021-03-17 14:47:32 -0500

    Trying to give num_threadsd before converting to pydra.

--- sandbox/antsJointFusionTest_smaller.py
+++ sandbox/antsJointFusionTest_smaller.py
@@ -2,7 +2,9 @@ from nipype.interfaces.ants import AntsJointFusion
 from pydra.tasks.nipype1.utils import Nipype1Task
 import pydra
 
-antsJointFusion_task = AntsJointFusion()
+antsJointFusion = AntsJointFusion()
+antsJointFusion.num_threads = 28
+antsJointFusion_task = Nipype1Task(antsJointFusion)
 antsJointFusion_task.inputs.atlas_image = [
     # ['/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/91300_2subject.nii.gz'],
     # ['/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/91626_2subject.nii.gz']
@@ -25,14 +27,14 @@ antsJointFusion_task.inputs.mask_image = "/localscratch/Users/cjohnson30/output_
 # antsJointFusion_task.inputs.mask_image = "/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/fixedImageROIAutoMask.nii.gz"
 antsJointFusion_task.inputs.out_label_fusion = "JointFusion_HDAtlas20_2015_label.nii.gz"
 antsJointFusion_task.inputs.verbose = True
-antsJointFusion_task.inputs.num_threads = 28
+# antsJointFusion_task.inputs.num_threads = 28
 
-antsJointFusion_workflow = Nipype1Task(antsJointFusion_task)
+# antsJointFusion_workflow = Nipype1Task(antsJointFusion_task)
 
 # print(antsJointFusion_task.cmdline)
 # result = antsJointFusion_task.run()
 # print(result)
 with pydra.Submitter(plugin="cf") as sub:
-    sub(antsJointFusion_workflow)
-result = antsJointFusion_workflow.result()
+    sub(antsJointFusion_task)
+result = antsJointFusion_task.result()
 print(result)
