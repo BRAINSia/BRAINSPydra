30019afd9b1d9b88305c2bb5f986e4c3afa83688
Chase Johnson <chaser.johnson27@gmail.com>
2021-03-17 14:15:41 -0500

    Trying to set threads in a pydra wrapped JointFusion interface.

--- sandbox/antsJointFusionTest_smaller.py
+++ sandbox/antsJointFusionTest_smaller.py
@@ -1,7 +1,8 @@
 from nipype.interfaces.ants import AntsJointFusion
+from pydra.tasks.nipype1.utils import Nipype1Task
 import pydra
 
-antsJointFusion_task = AntsJointFusion()
+antsJointFusion_task = Nipype1Task(AntsJointFusion())
 antsJointFusion_task.inputs.atlas_image = [
     # ['/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/91300_2subject.nii.gz'],
     # ['/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/91626_2subject.nii.gz']
@@ -24,13 +25,12 @@ antsJointFusion_task.inputs.mask_image = "/localscratch/Users/cjohnson30/output_
 # antsJointFusion_task.inputs.mask_image = "/mnt/c/2020_Grad_School/Research/output_dir/sub-052823_ses-43817_run-002_T1w/fixedImageROIAutoMask.nii.gz"
 antsJointFusion_task.inputs.out_label_fusion = "JointFusion_HDAtlas20_2015_label.nii.gz"
 antsJointFusion_task.inputs.verbose = True
-# antsJointFusion_task.set_default_num_threads(28)
 antsJointFusion_task.inputs.num_threads = 28
 
-# print(antsJointFusion_task.input_spec)
-# antsJointFusion_task.inputs.num_threads(56)
-# antsJointFusion_task.set_default_num_threads(24)
-
-print(antsJointFusion_task.cmdline)
-result = antsJointFusion_task.run()
+# print(antsJointFusion_task.cmdline)
+# result = antsJointFusion_task.run()
+# print(result)
+with pydra.Submitter(plugin="cf") as sub:
+    sub(antsJointFusion_task)
+result = antsJointFusion_task.result()
 print(result)
