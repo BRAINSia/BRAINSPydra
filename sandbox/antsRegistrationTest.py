1f205c94f56fce4aa7be597e456a1ac47709a5fe
Chase Johnson <chaser.johnson27@gmail.com>
2021-03-19 14:29:47 -0500

    Removed setting num_threads before inputs.

--- sandbox/antsRegistrationTest.py
+++ sandbox/antsRegistrationTest.py
@@ -6,7 +6,7 @@ from nipype.interfaces.ants import Registration
 
 antsRegistration_task = Registration()
 antsRegistration_task.set_default_num_threads(28)
-antsRegistration_task.inputs.num_threads = 28
+# antsRegistration_task.inputs.num_threads = 28
 antsRegistration_task = Nipype1Task(antsRegistration_task)
 
 # antsRegistration_task = Nipype1Task(Registration())
