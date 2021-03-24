2d97a4802dd6e5f8ad7e722edff630a5c06c0a87
Chase Johnson <chaser.johnson27@gmail.com>
2021-03-24 14:51:46 -0500

    Just get_values() instead of converting to list.

--- preliminary_pipeline5.py
+++ preliminary_pipeline5.py
@@ -911,7 +911,10 @@ if __name__ == '__main__':
             # If the files to be copied locally are nested in a dictionary, put the values of the dictionary in a list
             if type(cache_path) is dict:
                 print("\n\nHere\n\n")
-                cache_path_elements = list(cache_path.values())
+                if type(cache_path.values()) is dict:
+                    cache_path_elements = cache_path.values()
+                else:
+                    cache_path_elements = list(cache_path.values())
                 print(cache_path_elements)
                 cache_path = []
                 for cache_path_element in cache_path_elements:
@@ -1014,11 +1017,11 @@ if __name__ == '__main__':
     # The sink converts the cached files to output_dir, a location on the local machine
     sink_node = pydra.Workflow(name="sink_node", input_spec=['processed_files', 'input_data'], processed_files=processing_node.lzout.all_, input_data=source_node.lzin.input_data)
     sink_node.add(get_processed_outputs(name="get_processed_outputs", processed_dict=sink_node.lzin.processed_files))
-    sink_node.add(copy_from_cache(name="copy_from_cache3", output_dir=experiment_configuration['output_dir'], cache_path=sink_node.get_processed_outputs.lzout.out, input_data=sink_node.lzin.input_data).split("cache_path"))
+    sink_node.add(copy_from_cache(name="copy_from_cache4", output_dir=experiment_configuration['output_dir'], cache_path=sink_node.get_processed_outputs.lzout.out, input_data=sink_node.lzin.input_data).split("cache_path"))
     # sink_node.add(get_processed_outputs(name="get_post_processed_outputs", processed_dict=sink_node.lzin.post_processed_files))
     # sink_node.add(copy_from_cache(name="copy_from_cache2", output_dir=experiment_configuration['output_dir'], cache_path=sink_node.get_post_processed_outputs.lzout.out, input_data=sink_node.lzin.input_data).split("cache_path"))
     sink_node.set_output([
-        ("output_files1", sink_node.copy_from_cache3.lzout.out),
+        ("output_files1", sink_node.copy_from_cache4.lzout.out),
         # ("output_files2", sink_node.copy_from_cache2.lzout.out)
     ])
 
