329b5ad700f63d0b2a32e206743d57878df1b633
Chase Johnson <chaser.johnson27@gmail.com>
2021-03-24 20:24:55 -0500

    Incremented copy.

--- preliminary_pipeline5.py
+++ preliminary_pipeline5.py
@@ -1068,8 +1068,8 @@ if __name__ == '__main__':
 
 
     sink_node2 = pydra.Workflow(name="sink_node", input_spec=["output_directory"], output_directory=source_node.output_dir)
-    sink_node2.add(copy(name="copy2", source_output_dir=sink_node2.lzin.output_directory))
-    sink_node2.set_output([("files_out", sink_node2.copy2.lzout.out)])
+    sink_node2.add(copy(name="copy3", source_output_dir=sink_node2.lzin.output_directory))
+    sink_node2.set_output([("files_out", sink_node2.copy3.lzout.out)])
 
 
 
