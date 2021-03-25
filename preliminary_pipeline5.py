acdb6f44be3bccbdc3a137308bc223d565528abc
Chase Johnson <chaser.johnson27@gmail.com>
2021-03-24 19:40:33 -0500

    Globbing through output dir.

--- preliminary_pipeline5.py
+++ preliminary_pipeline5.py
@@ -1068,11 +1068,11 @@ if __name__ == '__main__':
         input_filename = Path(input_data.get('t1')).with_suffix('').with_suffix('').name
         file_output_dir = Path(source_output_dir) / Path(input_filename)
         file_output_dir.mkdir(parents=True, exist_ok=True)
-        p = Path(dir).glob("**/*")
+        p = Path(source_output_dir).glob("**/*")
         files = [x for x in p if x.is_file()]
         print(files)
         return files
-    sink_node2 = pydra.Workflow(name="sink_node3", input_spec=["output_directory", "input_data"], output_directory=source_node.output_dir, input_data=source_node.lzin.input_data)
+    sink_node2 = pydra.Workflow(name="sink_node4", input_spec=["output_directory", "input_data"], output_directory=source_node.output_dir, input_data=source_node.lzin.input_data)
     sink_node2.add(copy(name="copy2", source_output_dir=sink_node2.lzin.output_directory, input_data=sink_node2.lzin.input_data).split("source_output_dir"))
     sink_node2.set_output([("files_out", sink_node2.copy2.lzout.out)])
 
@@ -1087,7 +1087,7 @@ if __name__ == '__main__':
 
     # Set the output of the source node to the same as the output of the sink_node
     # source_node.set_output([("output_files", source_node.sink_node.lzout.pipline_output),])
-    source_node.set_output([("output_files", source_node.sink_node3.lzout.files_out)])
+    source_node.set_output([("output_files", source_node.sink_node4.lzout.files_out)])
 
     # source_node.output_dir = experiment_configuration['output_dir']
     # Run the entire workflow
