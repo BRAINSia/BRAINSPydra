b52903af63a2dfea72516e7856a5da2e31022881
Chase Johnson <chaser.johnson27@gmail.com>
2021-03-25 11:44:05 -0500

    Getting the input_data list.

--- preliminary_pipeline5.py
+++ preliminary_pipeline5.py
@@ -1106,7 +1106,7 @@ if __name__ == '__main__':
 
 
     # After processing all the files, copy the results to a local output directory
-    sessions = [sess_data['session'] for sess_data in input_data_dictionary]
+    sessions = [sess_data['session'] for sess_data in input_data_dictionary.get("input_data")]
     print(f"\n\n{sessions}\n\n")
 
     sink_node = pydra.Workflow(name="sink_node", input_spec=["output_directory", "input_data"],
