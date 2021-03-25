4dad2bff7845a341610fe86859355ccd23250994
Chase Johnson <chaser.johnson27@gmail.com>
2021-03-25 08:35:20 -0500

    Trying to create hard_link.

--- preliminary_pipeline5.py
+++ preliminary_pipeline5.py
@@ -1124,7 +1124,8 @@ if __name__ == '__main__':
             print(f"Copying {cache_filepath} to {output_filepath}")
             print(type(cache_filepath))
             print(type(output_filepath))
-            copyfile(cache_filepath, output_filepath)
+            output_filepath.link_to(cache_filepath)
+            # copyfile(cache_filepath, output_filepath)
             # if environment_configuration['hard_links']:
             #     output_filepath.link_to(cache_filepath)
             #     print(f"Hard linked {cache_filepath} to {output_filepath}")
