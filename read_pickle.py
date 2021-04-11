9ffdf93d1c33fa97ef68324ee29bd6fa9e279fd7
Chase Johnson <cjohnson30@uiowa.edu>
2021-04-11 15:19:16 -0500

    Made a config file for caudate.

--- read_pickle.py
+++ read_pickle.py
@@ -9,10 +9,14 @@ data = pickle.load(f)
 f.close()
 try:
     f = open(sys.argv[2], "a")
-    print(str(data))
-    print(str(data.get_output_field("stdout")))
-    print(str(data.get_output_field("stderr")))
-    f.write(str(data.get_output_field("stdout")))
+    print(data.processing_node_with_T2.output_dir)
+    print(data.processing_node_with_T2.inputs.input_data_with_T2)
+
+    # for out_dir in data.processing_node_with_T2.output_dir:
+
+    # print(str(data.get_output_field("stdout")))
+    # print(str(data.get_output_field("stderr")))
+    # f.write(str(data.get_output_field("stdout")))
 except:
     pass
 try:
