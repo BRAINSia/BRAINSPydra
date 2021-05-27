c4fcc3b6a371fdc9c5e3732cb3b1f73d44ad8aae
Chase Johnson <cjohnson30@uiowa.edu>
2021-05-26 23:59:06 -0500

    Writing to several different input_data_dicitonary files for job arrays.

--- extract_input_data.py
+++ extract_input_data.py
@@ -16,7 +16,39 @@ parser.add_argument(
     type=str,
     help="Path to the tsv file containing information on the best series for each session",
 )
-
+parser.add_argument(
+    "data_dictionary_dir",
+    type=str,
+    help="Path the the directory to store the generated input_data_dictionary__.json files",
+)
+parser.add_argument(
+    "pipeline_script",
+    type=str,
+    help="Path to the python pipeline script to run BAW using pydra",
+)
+parser.add_argument(
+    "experimental_config",
+    type=str,
+    help="Path to the experimental configuration json file defining settings for each of the BRAINSTools applications",
+)
+parser.add_argument(
+    "environmental_config",
+    type=str,
+    help="Path to the environmental configuration json file defining overall settings for the system being used",
+)
+parser.add_argument(
+    "--output_job_path",
+    type=str,
+    default="pipeline.job",
+    help="Output path to the job file that will be run to execute the pipeline on all the input data dictionary files",
+)
+parser.add_argument(
+    "--max_sessions_per_file",
+    type=int,
+    help="The number of sessions to be recorded per json file",
+    default=500,
+    required=False,
+)
 parser.add_argument(
     "--session_count",
     type=int,
@@ -44,16 +76,25 @@ sessions_regex = "*sub-*/ses-*/"
 sessions_dict = {"sessions_with_T2": [], "sessions_without_T2": []}
 
 p = Path(args.bids_path)
-total_sessions = len(list(p.glob(sessions_regex)))
+if args.session_count == -1:
+    total_sessions = len(list(p.glob(sessions_regex)))
+else:
+    total_sessions = args.session_count
+
+files_created = []
 sessions = p.glob(sessions_regex)
 counter = 1
 for session in sessions:
     session_id = f"{session.parent.name}_{session.name}"
-    print(f"{counter} / {total_sessions} Reading data from {str(session)}")
     if (
         counter > args.session_count and args.session_count != -1
     ):  # Only read args.session_count sessions unless it is -1, then read all sessions
         break
+    # if args.session_count == -1:
+    print(f"{counter} / {total_sessions} Reading data from {str(session)}")
+    # else:
+    # print(f"{counter} / {args.session_count} Reading data from {str(session)}")
+
     counter += 1
 
     inputVolumes = []
@@ -65,34 +106,40 @@ for session in sessions:
         bids_filename_obj = BIDSFilename(inputVolume)
         inputVolumeType = None
 
-        # Put the best T1 image at the beginning of the inputVolumes list and set its landmark file
-        if bids_filename_obj.attribute_dict["run"] == best_t1_by_session[session_id]:
-            if "T1w.nii.gz" in inputVolume.name:
-                if inputVolume.with_suffix("").with_suffix(".fcsv").exists():
-                    inputLandmarksEMSP = str(
-                        inputVolume.with_suffix("").with_suffix(".fcsv")
-                    )
-                else:
-                    inputLandmarksEMSP = None
-                inputVolumes.insert(0, str(inputVolume))
-                inputVolumeTypes.insert(0, "T1")
+        if session_id in best_t1_by_session:
+            # Put the best T1 image at the beginning of the inputVolumes list and set its landmark file
+            if (
+                bids_filename_obj.attribute_dict["run"]
+                == best_t1_by_session[session_id]
+            ):
+                if "T1w.nii.gz" in inputVolume.name:
+                    if inputVolume.with_suffix("").with_suffix(".fcsv").exists():
+                        inputLandmarksEMSP = str(
+                            inputVolume.with_suffix("").with_suffix(".fcsv")
+                        )
+                    else:
+                        inputLandmarksEMSP = None
+                    inputVolumes.insert(0, str(inputVolume))
+                    inputVolumeTypes.insert(0, "T1")
 
-        # If the current inputVolume is not the best for the session,
-        # add its information to the end of the input_data lists
+            # If the current inputVolume is not the best for the session,
+            # add its information to the end of the input_data lists
+            else:
+                if "T1w.nii.gz" in inputVolume.name:
+                    inputVolumeType = "T1"
+
+                elif "T2w.nii.gz" in inputVolume.name:
+                    inputVolumeType = "T2"
+                elif "PD.nii.gz" in inputVolume.name:
+                    inputVolumeType = "PD"
+                elif "FL.nii.gz" in inputVolume.name:
+                    inputVolumeType = "FL"
+
+                if inputVolumeType is not None:
+                    inputVolumes.append(str(inputVolume))
+                    inputVolumeTypes.append(inputVolumeType)
         else:
-            if "T1w.nii.gz" in inputVolume.name:
-                inputVolumeType = "T1"
-
-            elif "T2w.nii.gz" in inputVolume.name:
-                inputVolumeType = "T2"
-            elif "PD.nii.gz" in inputVolume.name:
-                inputVolumeType = "PD"
-            elif "FL.nii.gz" in inputVolume.name:
-                inputVolumeType = "FL"
-
-            if inputVolumeType is not None:
-                inputVolumes.append(str(inputVolume))
-                inputVolumeTypes.append(inputVolumeType)
+            print(f"Skipping {session_id}")
 
     if "T2" in inputVolumeTypes:
         sessions_dict["sessions_with_T2"].append(
@@ -112,12 +159,47 @@ for session in sessions:
                 "inputLandmarksEMSP": inputLandmarksEMSP,
             }
         )
+    if (
+        len(sessions_dict["sessions_without_T2"])
+        + len(sessions_dict["sessions_with_T2"])
+        == args.max_sessions_per_file
+    ):
+        output_file_name = Path(args.data_dictionary_dir) / Path(
+            f"input_data_dictionary_{counter-args.max_sessions_per_file}_{counter-1}.json"
+        )
+        with open(output_file_name, "w") as out_file:
+            json.dump(sessions_dict, out_file, indent=4)
+        print(
+            f"Wrote data from {args.max_sessions_per_file} sessions to {output_file_name}"
+        )
+        sessions_dict = {"sessions_with_T2": [], "sessions_without_T2": []}
+        files_created.append(output_file_name)
 
-if args.session_count == -1:
-    output_file_name = f"input_data_dictionary_{total_sessions}.json"
-else:
-    output_file_name = f"input_data_dictionary_{args.session_count}.json"
-with open(output_file_name, "w") as out_file:
-    json.dump(sessions_dict, out_file, indent=4)
+# if args.session_count == -1:
+#     output_file_name = f"input_data_dictionary_{total_sessions}.json"
+# else:
+#     output_file_name = f"input_data_dictionary_{args.session_count}.json"
+output_file_name = Path(args.data_dictionary_dir) / Path(
+    f"input_data_dictionary_{counter-(counter%args.max_sessions_per_file)}_{counter-1}.json"
+)
+if (
+    len(sessions_dict["sessions_with_T2"]) > 0
+    or len(sessions_dict["sessions_without_T2"]) > 0
+):
+    with open(output_file_name, "w") as out_file:
+        json.dump(sessions_dict, out_file, indent=4)
+    files_created.append(output_file_name)
+    print(
+        f"Wrote data from {total_sessions % args.max_sessions_per_file} sessions to {output_file_name}"
+    )
 
-print(f"Wrote data from {args.session_count} sessions to {output_file_name}")
+job_string = """#!/bin/sh
+case $SGE_TASK_ID in"""
+
+for index, input_data_dictionary_file in enumerate(files_created):
+    job_string += f"\n\t{index+1}) python {args.pipeline_script} {args.experimental_config} {args.environmental_config} {input_data_dictionary_file};;"
+job_string += "\nesac"
+
+with open(args.output_job_path, "w") as out_file:
+    out_file.write(job_string)
+print(f"Wrote job to {args.output_job_path}")
