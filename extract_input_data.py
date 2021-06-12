ab827c2f21b77cd1e4e5b2ed4690d382d982490d
Chase Johnson <cjohnson30@uiowa.edu>
2021-06-12 13:10:37 -0500

    Only using the saved sessions counter to accurately write the number of requested sessions.

--- extract_input_data.py
+++ extract_input_data.py
@@ -59,7 +59,14 @@ parser.add_argument(
 parser.add_argument(
     "--original_sessions_list_file",
     type=str,
-    help="The path to the list file where each line is a session processed in the original BAW predicthd run",
+    help="The path to a list file where each line is a session (sess-#####) processed in the original BAW predicthd run",
+    default="",
+    required=False,
+)
+parser.add_argument(
+    "--bad_sessions_list",
+    type=str,
+    help="The path to a list file where each line is a session (sess-####) that should not be processed",
     default="",
     required=False,
 )
@@ -80,7 +87,9 @@ with open(args.best_image_table) as fd:
 if args.original_sessions_list_file != "":
     with open(args.original_sessions_list_file) as fd:
         sessions_to_record = fd.read().splitlines()
-
+if args.bad_sessions_list != "":
+    with open(args.bad_sessions_list) as fd:
+        bad_sessions = fd.read().splitlines()
 
 sessions_regex = "*sub-*/ses-*/"
 
@@ -97,101 +106,118 @@ if args.max_sessions_per_file == -1:
 files_created = []
 saved_sessions_count = 0
 sessions = p.glob(sessions_regex)
-counter = 1
+# counter = 1
 for session in sessions:
-    print(f"{counter} / {total_sessions} Reading data from {str(session)}")
-    counter += 1
-    if Path(session).name in sessions_to_record:
-        session_id = f"{session.parent.name}_{session.name}"
-        if (
-            counter > args.session_count and args.session_count != -1
-        ):  # Only read args.session_count sessions unless it is -1, then read all sessions
-            break
-        # if args.session_count == -1:
-        # else:
-        # print(f"{counter} / {args.session_count} Reading data from {str(session)}")
-
-
-        inputVolumes = []
-        inputVolumeTypes = []
-        inputLandmarksEMSP = None
-
-        nifty_files = session.glob("*.nii.gz")
-        for inputVolume in nifty_files:
-            if "BAD" not in inputVolume.name:
-                bids_filename_obj = BIDSFilename(inputVolume)
-                inputVolumeType = None
-
-                if session_id in best_t1_by_session:
-                    # Put the best T1 image at the beginning of the inputVolumes list and set its landmark file
-                    if (
-                        bids_filename_obj.attribute_dict["run"]
-                        == best_t1_by_session[session_id]
-                    ):
-                        if "T1w.nii.gz" in inputVolume.name:
-                            if inputVolume.with_suffix("").with_suffix(".fcsv").exists():
-                                inputLandmarksEMSP = str(
-                                    inputVolume.with_suffix("").with_suffix(".fcsv")
-                                )
-                            else:
-                                inputLandmarksEMSP = None
-                            inputVolumes.insert(0, str(inputVolume))
-                            inputVolumeTypes.insert(0, "T1")
-
-                    # If the current inputVolume is not the best for the session,
-                    # add its information to the end of the input_data lists
-                    else:
-                        if "T1w.nii.gz" in inputVolume.name:
-                            inputVolumeType = "T1"
-
-                        elif "T2w.nii.gz" in inputVolume.name:
-                            inputVolumeType = "T2"
-                        elif "PD.nii.gz" in inputVolume.name:
-                            inputVolumeType = "PD"
-                        elif "FL.nii.gz" in inputVolume.name:
-                            inputVolumeType = "FL"
-
-                        if inputVolumeType is not None:
-                            inputVolumes.append(str(inputVolume))
-                            inputVolumeTypes.append(inputVolumeType)
-                else:
-                    print(f"Skipping {session_id}")
-
-        if "T1" in inputVolumeTypes:
-            saved_sessions_count += 1
-            if "T2" in inputVolumeTypes:
-                sessions_dict["sessions_with_T2"].append(
-                    {
-                        "session": session_id,
-                        "inputVolumes": inputVolumes,
-                        "inputVolumeTypes": inputVolumeTypes,
-                        "inputLandmarksEMSP": None,
-                    }
-                )
-            else:
-                sessions_dict["sessions_without_T2"].append(
-                    {
-                        "session": session_id,
-                        "inputVolumes": inputVolumes,
-                        "inputVolumeTypes": inputVolumeTypes,
-                        "inputLandmarksEMSP": None,
-                    }
-                )
+    if args.original_sessions_list_file == "" or Path(session).name in sessions_to_record:
+        if args.bad_sessions_list == "" or Path(session).name not in bad_sessions:
+            session_id = f"{session.parent.name}_{session.name}"
             if (
-                len(sessions_dict["sessions_without_T2"])
-                + len(sessions_dict["sessions_with_T2"])
-                == args.max_sessions_per_file
-            ):
-                output_file_name = Path(args.data_dictionary_dir) / Path(
-                    f"input_data_dictionary_{saved_sessions_count+1-args.max_sessions_per_file}_{saved_sessions_count}.json"
-                )
-                with open(output_file_name, "w") as out_file:
-                    json.dump(sessions_dict, out_file, indent=4)
-                print(
-                    f"Wrote data from {args.max_sessions_per_file} sessions to {output_file_name}"
-                )
-                sessions_dict = {"sessions_with_T2": [], "sessions_without_T2": []}
-                files_created.append(output_file_name)
+                saved_sessions_count >= args.session_count and args.session_count != -1
+            ):  # Only read args.session_count sessions unless it is -1, then read all sessions
+                break
+            # if args.session_count == -1:
+            # else:
+            # print(f"{counter} / {args.session_count} Reading data from {str(session)}")
+            print(f"{saved_sessions_count+1} / {total_sessions} Reading data from {str(session)}")
+
+
+            inputVolumes = []
+            inputVolumeTypes = []
+
+            
+            nifty_files_in_anat = list(session.glob("anat/*.nii.gz"))
+            # print(f"nfia: {nifty_files_in_anat}")
+            nifty_files_not_in_anat = list(session.glob("*.nii.gz"))
+            # print(f"nfnia: {nifty_files_not_in_anat}")
+            nifty_files = nifty_files_in_anat + nifty_files_not_in_anat
+            # print(f"nf: {nifty_files}")
+
+
+            for inputVolume in nifty_files:
+                if "BAD" not in inputVolume.name:
+                    bids_filename_obj = BIDSFilename(inputVolume)
+                    inputVolumeType = None
+
+                    if session_id in best_t1_by_session:
+                        # print(f"attr_dict: {bids_filename_obj.attribute_dict}")
+                        # Put the best T1 image at the beginning of the inputVolumes list and set its landmark file
+                        if (
+                            "run" in bids_filename_obj.attribute_dict and 
+                            bids_filename_obj.attribute_dict["run"]
+                            == best_t1_by_session[session_id]
+                        ):
+                            if "T1w.nii.gz" in inputVolume.name:
+                                inputVolumes.insert(0, str(inputVolume))
+                                inputVolumeTypes.insert(0, "T1")
+
+                        # If the current inputVolume is not the best for the session,
+                        # add its information to the end of the input_data lists
+                        else:
+                            if "T1w.nii.gz" in inputVolume.name:
+                                inputVolumeType = "T1"
+
+                            elif "T2w.nii.gz" in inputVolume.name:
+                                inputVolumeType = "T2"
+                            elif "PD.nii.gz" in inputVolume.name:
+                                inputVolumeType = "PD"
+                            elif "FL.nii.gz" in inputVolume.name:
+                                inputVolumeType = "FL"
+
+                            if inputVolumeType is not None:
+                                inputVolumes.append(str(inputVolume))
+                                inputVolumeTypes.append(inputVolumeType)
+                    else:
+                        print(f"Skipping {session_id}")
+
+            if "T1" in inputVolumeTypes:
+                first_t1_index = inputVolumeTypes.index("T1")
+                first_t1 = Path(inputVolumes[first_t1_index])
+                if first_t1.with_suffix('').with_suffix('.fcsv').exists():
+                    inputLandmarksEMSP = str(first_t1.with_suffix('').with_suffix('.fcsv'))
+                else:
+                    inputLandmarksEMSP = None
+                print("Adding to saved_sessions")
+                saved_sessions_count += 1
+                if "T2" in inputVolumeTypes:
+                    sessions_dict["sessions_with_T2"].append(
+                        {
+                            "session": session_id,
+                            "inputVolumes": inputVolumes,
+                            "inputVolumeTypes": inputVolumeTypes,
+                            "inputLandmarksEMSP": inputLandmarksEMSP,
+                        }
+                    )
+                else:
+                    sessions_dict["sessions_without_T2"].append(
+                        {
+                            "session": session_id,
+                            "inputVolumes": inputVolumes,
+                            "inputVolumeTypes": inputVolumeTypes,
+                            "inputLandmarksEMSP": inputLandmarksEMSP,
+                        }
+                    )
+                if (
+                    len(sessions_dict["sessions_without_T2"])
+                    + len(sessions_dict["sessions_with_T2"])
+                    == args.max_sessions_per_file
+                ):
+                    output_file_name = Path(args.data_dictionary_dir) / Path(
+                        f"input_data_dictionary_{saved_sessions_count+1-args.max_sessions_per_file}_{saved_sessions_count}.json"
+                    )
+                    with open(output_file_name, "w") as out_file:
+                        json.dump(sessions_dict, out_file, indent=4)
+                    print(
+                        f"Wrote data from {args.max_sessions_per_file} sessions to {output_file_name}"
+                    )
+                    sessions_dict = {"sessions_with_T2": [], "sessions_without_T2": []}
+                    files_created.append(output_file_name)
+        else:
+            print("in bad sessions")
+    # counter += 1
+
+    
+
+
 
 output_file_name = Path(args.data_dictionary_dir) / Path(
     f"input_data_dictionary_{saved_sessions_count+1-(saved_sessions_count%args.max_sessions_per_file)}_{saved_sessions_count}.json"
